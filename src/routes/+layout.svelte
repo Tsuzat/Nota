bf60ed9e50751d4d96ef61f474ab1bddf4b8a905
Tsuzat <dev.tsuzat@gmail.com>
2024-12-11 22:27:11 +0530

    add: UI components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
+	import '@fontsource-variable/dm-sans';
 	import '../app.css';
 	let { children } = $props();
 </script>
+
+<ModeWatcher />
 
 {@render children()}
