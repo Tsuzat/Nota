7ada866b846f6257f2b85797be5f470ee598fb1b
Tsuzat <dev.tsuzat@gmail.com>
2024-12-13 09:29:04 +0530

    add: +layout.svelte and +page.svelte with basic application layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,18 @@
 	import '@fontsource-variable/dm-sans';
 	import '../app.css';
 	import SidebarPage from '$lib/components/customs/sidebar/sidebar-page.svelte';
+	import { onMount } from 'svelte';
+	import { initializeApplication } from '$lib/utils';
 	let { children } = $props();
+
+	onMount(async () => {
+		// load the data
+		initializeApplication();
+	});
 </script>
 
 <ModeWatcher />
 
-<SidebarPage />
-
-{@render children()}
+<SidebarPage>
+	{@render children()}
+</SidebarPage>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
 <script lang="ts">
+	import NavActions from '$lib/components/customs/sidebar/nav-actions.svelte';
+	import ShadEditor from '$lib/components/shad-editor/shad-editor.svelte';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb';
+	import { Separator } from '$lib/components/ui/separator';
+	import * as Sidebar from '$lib/components/ui/sidebar';
 </script>
+
+<main>
+	<header class="flex h-14 shrink-0 items-center gap-2">
+		<div class="flex flex-1 items-center gap-2 px-3">
+			<Sidebar.Trigger />
+			<Separator orientation="vertical" class="mr-2 h-4" />
+			<Breadcrumb.Root>
+				<Breadcrumb.List>
+					<Breadcrumb.Item>
+						<Breadcrumb.Page class="line-clamp-1"
+							>Project Management & Task Tracking</Breadcrumb.Page
+						>
+					</Breadcrumb.Item>
+				</Breadcrumb.List>
+			</Breadcrumb.Root>
+		</div>
+		<div class="ml-auto px-3">
+			<NavActions />
+		</div>
+	</header>
+	<div class="flex flex-1 flex-col gap-4 p-2">
+		<div class="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl"></div>
+		<div class="mx-auto h-full w-full max-w-3xl">
+			<ShadEditor />
+		</div>
+	</div>
+</main>
