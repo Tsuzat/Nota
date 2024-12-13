6b030507f737b227477a12db44f70789345b6f5a
Tsuzat <dev.tsuzat@gmail.com>
2024-12-13 09:29:36 +0530

    add: InitialiseApp function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { initializeDatabase } from './database/sqldb';
+import { error } from '@tauri-apps/plugin-log';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export async function initializeApplication() {
+	await initializeDatabase();
 }
