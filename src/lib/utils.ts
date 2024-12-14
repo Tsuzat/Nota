f838c9844a781c85c82162a7c2e778fab808b20b
Tsuzat <dev.tsuzat@gmail.com>
2024-12-14 22:48:29 +0530

    update: Utils.ts with app initializer

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { initializeDatabase } from './database/sqldb';
-import { error } from '@tauri-apps/plugin-log';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
