b24e6aff390160e26d11f5f88d552797a275c30b
Tsuzat <dev.tsuzat@gmail.com>
2024-12-15 22:55:13 +0530

    add: WORKSPACES global constant

--- src/lib/contants.ts
+++ src/lib/contants.ts
@@ -1,8 +1,9 @@
 import { writable } from 'svelte/store';
 import type { WorkSpaceDB } from './database/workspace';
+import type { NotesDB } from './database/notes';
 
 /** Global Variables For showing new work space dialog */
 export const OPEN_NEW_WORKSPACE_DIALOG = writable(false);
 
-/** Global Variables For keep tack of workspaces */
-export const WORKSPACES = writable<WorkSpaceDB[]>([]);
+/** Global Variables For keep tack of workspaces and it's notes */
+export const WORKSPACES = writable<Map<WorkSpaceDB, NotesDB[]>>(new Map());
