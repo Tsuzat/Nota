92c86a06ab29270e57d7fc1eb0936a7d13d47163
Tsuzat <dev.tsuzat@gmail.com>
2024-12-15 22:55:29 +0530

    add: hooks.client.ts with app initilizer

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,6 +1,15 @@
+import { page } from '$app/stores';
 import { initializeDatabase } from '$lib/database/sqldb';
+import { getCurrentWindow } from '@tauri-apps/api/window';
 
 (async () => {
 	// Initialize the database
 	initializeDatabase();
+
+	// Show URL on appwindow title
+	page.subscribe((page) => {
+		if (!page.url || !page.url.pathname) return;
+		const window = getCurrentWindow();
+		window.setTitle(`Nota - ${page.url.pathname}`);
+	});
 })();
