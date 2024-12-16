7bd84a6bf1e9bd120056e946962cc9127e7d7773
Tsuzat <dev.tsuzat@gmail.com>
2024-12-16 21:07:33 +0530

    add: OS information global constant

--- src/lib/contants.ts
+++ src/lib/contants.ts
@@ -1,9 +1,13 @@
 import { writable } from 'svelte/store';
 import type { WorkSpaceDB } from './database/workspace';
 import type { NotesDB } from './database/notes';
+import { platform } from '@tauri-apps/plugin-os';
 
 /** Global Variables For showing new work space dialog */
 export const OPEN_NEW_WORKSPACE_DIALOG = writable(false);
 
 /** Global Variables For keep tack of workspaces and it's notes */
 export const WORKSPACES = writable<Map<WorkSpaceDB, NotesDB[]>>(new Map());
+
+/** Global variable for keep track of current os */
+export const OS = platform();
