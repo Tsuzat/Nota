6c33ed978b12bdd78a38511cdd83ca0c88c700f6
Tsuzat <dev.tsuzat@gmail.com>
2024-12-18 23:13:52 +0530

    add: Tooltip component for quick and less verbose implementation

---
+++ src/lib/components/customs/tooltip.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import * as Tooltip from '$lib/components/ui/tooltip';
+	import { Content } from '../ui/dropdown-menu';
+
+	interface Props {
+		text: string;
+		children: any;
+	}
+	let { text, children }: Props = $props();
+</script>
+
+<Tooltip.Root delayDuration={0}>
+	<Tooltip.Trigger>
+		{@render children()}
+	</Tooltip.Trigger>
+	<Tooltip.Content class="bg-background text-foreground text-sm rounded p-1 border">
+		<p>{text}</p>
+	</Tooltip.Content>
+</Tooltip.Root>
