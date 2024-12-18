c2cf64972367a1f554fada45f3b858e3d3f6aa1e
Tsuzat <dev.tsuzat@gmail.com>
2024-12-18 23:10:39 +0530

    feat: add emojipicker for icons

---
+++ src/lib/components/icons/emojipicker.svelte
@@ -0,0 +1,61 @@
+<script lang="ts" module>
+	export interface Emojis {
+		[key: string]: EmojiItem[];
+	}
+
+	export interface EmojiItem {
+		name: string;
+		emoji: string;
+	}
+</script>
+
+<script lang="ts">
+	import emojisRaw from '$lib/assets/jsons/emojis.json';
+	import Tooltip from '../customs/tooltip.svelte';
+	import * as Tabs from '$lib/components/ui/tabs';
+
+	interface Props {
+		searchTerm?: string;
+		onSelect?: (emoji: string) => void;
+	}
+
+	let emojis: Emojis = $state(emojisRaw);
+	let categories = Object.keys(emojis);
+	let selectedCatergory = $state(categories[0]);
+
+	const categoriesEmojis: Record<string, string> = {
+		'Smileys & Emotion': '🙂',
+		Component: '🧩',
+		'People & Body': '👨‍👩‍👧‍👦',
+		'Animals & Nature': '🐶',
+		'Food & Drink': '🍔',
+		'Travel & Places': '🌍',
+		Activities: '🏃‍♂️',
+		Objects: '🚗',
+		Symbols: '❤️',
+		Flags: '🏳️'
+	};
+
+	let { searchTerm = $bindable(''), onSelect }: Props = $props();
+</script>
+
+<Tabs.Root bind:value={selectedCatergory}>
+	<Tabs.List class="w-96 flex items-center justify-evenly p-1">
+		{#each categories as catergory}
+			<Tabs.Trigger value={catergory} class="text-[1.25rem] p-1"
+				>{categoriesEmojis[catergory]}</Tabs.Trigger
+			>
+		{/each}
+	</Tabs.List>
+	{#each categories as catergory}
+		<Tabs.Content value={catergory} class="h-96 w-96 p-1 overflow-auto">
+			<div class="flex flex-wrap gap-1">
+				{#each emojis[catergory] as emoji}
+					<Tooltip text={emoji.name}>
+						<span class="text-xl">{emoji.emoji}</span>
+					</Tooltip>
+				{/each}
+			</div>
+		</Tabs.Content>
+	{/each}
+</Tabs.Root>
