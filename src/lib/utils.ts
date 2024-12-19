b4c500f08370b5fd4a02a7a87ed0fe5bdb25390f
Tsuzat <dev.tsuzat@gmail.com>
2024-12-19 11:23:20 +0530

    add: Update Global Workspaces and Favorites

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { initializeDatabase } from './database/sqldb';
+import { FAVORITE_NOTES, WORKSPACES } from './contants';
+import type { WorkSpaceDB } from './database/workspace';
+import type { NotesDB } from './database/notes';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -8,4 +11,31 @@ export function cn(...inputs: ClassValue[]) {
 
 export async function initializeApplication() {
 	await initializeDatabase();
+}
+
+/**
+ * Updates the global variable WORKSPACES
+ * @param workspace WorkspaceDB - Workspace to be updated
+ */
+export async function updateWorkspaceDB(workspace: WorkSpaceDB) {
+	WORKSPACES.update((workspaces) => {
+		const workspaceEntry = Array.from(workspaces.entries()).find(
+			([key]) => key.id === workspace.id
+		);
+		if (!workspaceEntry) return workspaces;
+		const [originalWorkspace, notes] = workspaceEntry;
+		workspaces.delete(originalWorkspace);
+		workspaces.set(workspace, notes);
+		return workspaces;
+	});
+}
+
+/**
+ * Update the global variable FAVORITE_NOTES with the new note
+ * @param note NotesDB - Note to be updated
+ */
+export async function updateFavoriteNote(note: NotesDB) {
+	FAVORITE_NOTES.update((notes) => {
+		return notes.map((n) => (n.id === note.id ? note : n));
+	});
 }
