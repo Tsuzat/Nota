b670ade3dd674fa1fd107dd11cacaba450976e75
Tsuzat <dev.tsuzat@gmail.com>
2024-12-28 16:20:15 +0530

    feat: Add trashed notes component with restore and permanent delete functionality

---
+++ src/lib/components/customs/tiles/trashed-notes.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { permanentlyDeleteNotes, updateNotesDB, type NotesDB } from '$lib/database/notes';
+	import { Trash2, Undo2 } from 'lucide-svelte';
+	import Tooltip from '../tooltip.svelte';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { updateNOTES } from '$lib/utils';
+
+	interface Props {
+		notes: NotesDB;
+	}
+	let { notes }: Props = $props();
+	async function deleteNote() {
+		permanentlyDeleteNotes(notes);
+	}
+	async function restore() {
+		notes.trashed = false;
+		updateNotesDB(notes);
+		updateNOTES(notes);
+	}
+</script>
+
+<div
+	class="p-1 flex items-center text-sm justify-between cursor-pointer hover:bg-muted/50 hover:rounded"
+>
+	<span>
+		<span>{notes.icon}</span>
+		<span>{notes.name}</span>
+	</span>
+	<span>
+		<Tooltip text="Restore">
+			<Button variant="ghost" class="size-6 p-0 rounded" onclick={restore}>
+				<Undo2 />
+			</Button>
+		</Tooltip>
+		<Tooltip text="Delete">
+			<Button variant="ghost" class="size-6 p-0 rounded" onclick={deleteNote}>
+				<Trash2 />
+			</Button>
+		</Tooltip>
+	</span>
+</div>
