4f409d2deba08de54ddd76977afbe703fadf3706
Tsuzat <dev.tsuzat@gmail.com>
2025-01-01 17:52:03 +0530

    feat: Enhance image insertion popover with tabs for URL and local file selection

--- src/lib/components/shad-editor/icons/image.svelte
+++ src/lib/components/shad-editor/icons/image.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
-	import { ChevronDown, Image, X } from 'lucide-svelte';
+	import { ChevronDown, Image, Link, Upload, X } from 'lucide-svelte';
 	import { type Editor } from '@tiptap/core';
 	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
-	import { Button } from '$lib/components/ui/button/index.js';
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
 	import * as Popover from '$lib/components/ui/popover/index.js';
 	import { cn } from '$lib/utils.js';
 	import { Input } from '$lib/components/ui/input';
+	import * as Tabs from '$lib/components/ui/tabs/index.js';
 	import { convertFileSrc } from '@tauri-apps/api/core';
 	import { pictureDir } from '@tauri-apps/api/path';
 	import { open } from '@tauri-apps/plugin-dialog';
+	import { Label } from '$lib/components/ui/label';
+	import { slide } from 'svelte/transition';
+	import { quartIn } from 'svelte/easing';
 
 	let { editor }: { editor: Editor } = $props();
+
+	let imageUrl: string | undefined = $state(undefined);
+	let open_ = $state(false);
 
 	async function handleLocalFile() {
 		const image = await open({
@@ -22,13 +29,21 @@
 		if (image === null) return;
 		const src = convertFileSrc(image);
 		editor.chain().focus().setImage({ src }).run();
+		handleClose(false);
+	}
+
+	function handleClose(open: boolean) {
+		if (!open) {
+			open_ = false;
+			imageUrl = undefined;
+		}
 	}
 </script>
 
 <Tooltip.Provider delayDuration={100}>
 	<Tooltip.Root>
 		<Tooltip.Trigger>
-			<Popover.Root>
+			<Popover.Root bind:open={open_} onOpenChange={handleClose}>
 				<Popover.Trigger>
 					<Button
 						variant="ghost"
@@ -38,32 +53,69 @@
 						<ChevronDown class="size-3 text-muted-foreground" />
 					</Button>
 				</Popover.Trigger>
-				<Popover.Content class="bg-popover shadow-lg *:my-2">
-					<div class="flex items-center justify-between">
-						<h1 class="text-xl font-bold">Image</h1>
-						<Popover.Close>
-							<X class="size-4 text-muted-foreground" />
-						</Popover.Close>
-					</div>
-					<p>Insert image url</p>
-					<Input
-						placeholder="Enter image url..."
-						type="url"
-						onchange={(e) => {
-							if (e !== null && e.target !== null) {
-								//@ts-ignore
-								editor.chain().focus().setImage({ src: e.target.value }).run();
-							}
-						}}
-						class="w-full"
-					/>
-					<p class="font-bold">OR</p>
-					<p>Pick an Image</p>
-					<Button variant="ghost" class="h-fit w-full bg-muted/50 p-0" onclick={handleLocalFile}>
-						<div class="flex w-full items-center justify-start p-4">
-							<span>Choose from your device</span>
-						</div>
-					</Button>
+				<Popover.Content class="bg-popover shadow-lg *:my-2 w-96 p-2">
+					<Popover.Close
+						class={buttonVariants({
+							variant: 'ghost',
+							class: 'size-4 p-0 rounded-sm fixed top-0 right-0 m-2 z-50'
+						})}
+					>
+						<X class="size-4" />
+					</Popover.Close>
+					<Tabs.Root value="link" class="">
+						<Tabs.List>
+							<Tabs.Trigger value="link">
+								<Link class="size-4 mr-2" />
+								<span>Image Link</span>
+							</Tabs.Trigger>
+							<Tabs.Trigger value="local">
+								<Upload class="size-4 mr-2" />
+								<span>Local Image</span>
+							</Tabs.Trigger>
+						</Tabs.List>
+						<Tabs.Content value="link">
+							<div>
+								<div class="*:my-2">
+									<Label for="image">Image Link</Label>
+									<div class="relative flex">
+										<Input
+											bind:value={imageUrl}
+											placeholder="Image URL https://example.com/image.jpg"
+										/>
+										<Button
+											variant="ghost"
+											onclick={() => (imageUrl = undefined)}
+											class="size-4 p-0 absolute right-2 top-2.5"
+										>
+											<X class="size-3" />
+										</Button>
+									</div>
+								</div>
+								{#if imageUrl && imageUrl.trim() !== ''}
+									<div class="*:my-2" transition:slide={{ duration: 300, easing: quartIn }}>
+										<h3 class="text-xl font-medium">Image Preview</h3>
+										<img src={imageUrl} alt="Preview" class="size-full rounded" />
+										<Button
+											class="ml-auto"
+											onclick={() => {
+												if (imageUrl !== undefined) {
+													editor.chain().focus().setImage({ src: imageUrl }).run();
+													imageUrl = undefined;
+													open_ = false;
+												}
+											}}>Insert</Button
+										>
+									</div>
+								{/if}
+							</div>
+						</Tabs.Content>
+						<Tabs.Content value="local">
+							<Button variant="outline" class="w-full h-32 mt-4" onclick={handleLocalFile}>
+								<Upload class="size-4 mr-2" />
+								<span>Drag and Drop or Select Files</span>
+							</Button>
+						</Tabs.Content>
+					</Tabs.Root>
 				</Popover.Content>
 			</Popover.Root>
 		</Tooltip.Trigger>
