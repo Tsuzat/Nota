e1a550b836fc2ecb51ac39ab985b4d5dee8fd00e
Tsuzat <dev.tsuzat@gmail.com>
2025-01-03 14:50:39 +0530

    feat: Add lucide-icon-render component for dynamic icon rendering

---
+++ src/lib/components/icons/lucide-icon-render.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import * as icons from 'lucide-svelte';
+	interface Props {
+		icon: string;
+		class?: string;
+	}
+	let { icon, class: className = '' }: Props = $props();
+
+	const Icon = icons[icon];
+</script>
+
+<Icon class={className} />
