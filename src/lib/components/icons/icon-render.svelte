f551c26826ce948c6329d8c30fd8c4e1fbfdbc63
Tsuzat <dev.tsuzat@gmail.com>
2025-01-03 13:38:55 +0530

    feat: Add icon-render component to handle different icon types

---
+++ src/lib/components/icons/icon-render.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { getIconType } from '$lib/utils';
+	import { icons } from 'lucide-svelte';
+
+	interface Props {
+		icon: string;
+		class?: string;
+	}
+	let { icon, class: className = '' }: Props = $props();
+
+	const iconType = getIconType(icon);
+</script>
+
+{#if iconType === 'emoji'}
+	<span class={className}>{icon}</span>
+{:else if iconType === 'lucide'}
+	{@const Icon = icons[icon]}
+	<Icon class={className} />
+{:else}
+	<img src={icon} alt="Icon" srcset="" />
+{/if}
