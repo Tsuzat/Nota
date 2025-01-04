94b28e13f52dff23ee0596a86ecf171fc559d230
Tsuzat <dev.tsuzat@gmail.com>
2025-01-04 12:02:49 +0530

    refactor: Improve update download notifications and simplify logging

--- src/lib/updater.ts
+++ src/lib/updater.ts
@@ -24,32 +24,26 @@ export async function checkUpdate() {
 }
 
 export async function downloadAndInstall(update: Update) {
-	console.log(`found update ${update.version} from ${update.date} with notes ${update.body}`);
-	const id = toast.info(`Update ${update.version} is available. Downloading...`, {
-		description: `Found update ${update.version} from ${update.date} with notes ${update.body}`
-	});
+	const id = toast.info(`Update ${update.version} is available. Downloading...`);
 	let downloaded = 0;
 	let contentLength = 0;
 	await update.downloadAndInstall((event) => {
 		switch (event.event) {
 			case 'Started':
 				contentLength = (event.data.contentLength ?? 0) / 1e6;
-				console.log(`started downloading ${event.data.contentLength} bytes`);
 				toast.info(`Downloading update ${update.version}...`, {
 					id,
-					description: `Started downloading ${contentLength} MB`
+					description: `Started downloading ${contentLength.toFixed(2)} MB`
 				});
 				break;
 			case 'Progress':
 				downloaded += event.data.chunkLength;
-				console.log(`downloaded ${downloaded} from ${contentLength}`);
 				toast.info(`Downloading update ${update.version}...`, {
 					id,
-					description: `Downloaded ${downloaded / 1e6} / ${contentLength} MB`
+					description: `Downloaded ${(downloaded / 1e6).toFixed(2)} / ${contentLength.toFixed(2)} MB`
 				});
 				break;
 			case 'Finished':
-				console.log('download finished');
 				toast.success(`Update ${update.version} downloaded. Installing...`, {
 					id,
 					description: 'Download finished'
