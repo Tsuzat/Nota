9a08d96c4e1054f0cb19cc4c9f9329ac6fd01b07
Tsuzat <dev.tsuzat@gmail.com>
2025-01-07 14:00:18 +0530

    feat: Enhance command bar with home navigation and keyboard instructions

--- src/lib/components/customs/dialogs/commandbar.svelte
+++ src/lib/components/customs/dialogs/commandbar.svelte
@@ -2,6 +2,7 @@
 	import { goto } from '$app/navigation';
 	import * as Command from '$lib/components/ui/command';
 	import { NOTES, OPEN_COMMAND_BAR } from '$lib/contants';
+	import { Home } from 'lucide-svelte';
 
 	function handleKeydown(e: KeyboardEvent) {
 		if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
@@ -13,10 +14,14 @@
 
 <svelte:document onkeydown={handleKeydown} />
 
-<Command.Dialog bind:open={$OPEN_COMMAND_BAR}>
+<Command.Dialog bind:open={$OPEN_COMMAND_BAR} class="p-2">
 	<Command.Input placeholder="Type a command or search..." />
 	<Command.List>
 		<Command.Empty>No results found.</Command.Empty>
+		<Command.Item value="home" keywords={['home', '/', 'homepage']} onclick={() => goto('/')}>
+			<Home />
+			<span>Home</span>
+		</Command.Item>
 		<Command.Group heading="All Notes">
 			{#each $NOTES as notes (notes.id)}
 				<Command.Item value={notes.id} keywords={[notes.name]} onclick={() => goto(`/${notes.id}`)}>
@@ -26,4 +31,16 @@
 			{/each}
 		</Command.Group>
 	</Command.List>
+	<div class="text-sm border-t py-1 text-center inline-flex justify-between">
+		<span class="text-muted-foreground"> <span class="key">↵</span> to select</span>
+		<span class="text-muted-foreground">
+			<span class="key">↑</span> and <span class="key">↓</span> to navigate</span
+		>
+	</div>
 </Command.Dialog>
+
+<style>
+	.key {
+		@apply bg-muted rounded-sm py-0.5 px-1;
+	}
+</style>
