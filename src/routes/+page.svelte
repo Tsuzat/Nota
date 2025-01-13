f656374f7b210f90e54522cfbf16f4c7afba38b7
Tsuzat <dev.tsuzat@gmail.com>
2025-01-13 13:11:38 +0530

    add: Enhance main page with app logo, welcome message, and recent notes display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-	import * as Breadcrumb from '$lib/components/ui/breadcrumb';
+	import { goto } from '$app/navigation';
+	import src from '$lib/assets/static/icon.png';
+	import Navigation from '$lib/components/customs/navigation.svelte';
+	import { Button } from '$lib/components/ui/button';
 	import { Separator } from '$lib/components/ui/separator';
 	import * as Sidebar from '$lib/components/ui/sidebar';
-	import { APPWINDOW } from '$lib/contants';
+	import { APPWINDOW, RECENT_NOTES } from '$lib/contants';
+	import { X } from 'lucide-svelte';
 	import { onMount } from 'svelte';
 
 	onMount(async () => {
@@ -15,10 +19,40 @@
 		<div class="flex flex-1 items-center gap-2 px-3">
 			<Sidebar.Trigger />
 			<Separator orientation="vertical" class="mr-2 h-4" />
+			<Navigation />
 		</div>
 	</header>
 	<div class="flex flex-1 flex-col gap-4 p-2">
-		<div class="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl"></div>
-		<div class="mx-auto h-56 w-full max-w-3xl bg-muted/50 rounded-xl"></div>
+		<div class="mx-auto h-24 w-full max-w-3xl rounded-xl flex flex-col items-center justify-around">
+			<div class="inline-flex items-center justify-center gap-4">
+				<img {src} alt="App Logo" srcset="App Logo" class="size-8" />
+				<span class="text-2xl font-bold">N O T A</span>
+			</div>
+			<div class="font-medium">Welcome to Nota - Your Note Taking App</div>
+		</div>
+		<div class="mx-auto w-full max-w-3xl rounded-xl p-4">
+			<div class="text-sm text-muted-foreground">
+				RECENT NOTES
+				<span class="text-sm text-foreground font-medium">{$RECENT_NOTES.size}</span>
+			</div>
+			<div class="flex items-center gap-2 p-2 rounded-xl flex-wrap">
+				{#each $RECENT_NOTES as note}
+					<Button
+						variant="outline"
+						size="lg"
+						class="flex items-center"
+						onclick={() => {
+							goto(`/${note.id}`);
+						}}
+					>
+						<span class="text-xl">{note.name}</span>
+						<span>{note.name}</span>
+						<Button variant="ghost" size="icon" class="size-6 ml-auto">
+							<X />
+						</Button>
+					</Button>
+				{/each}
+			</div>
+		</div>
 	</div>
 </main>
