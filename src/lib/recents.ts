f22eb03363de0835ad986e4eb39e8bb6967c2ddb
Tsuzat <dev.tsuzat@gmail.com>
2025-01-13 15:47:13 +0530

    refactor: Convert Recents class to writable store and simplify note management functions

--- src/lib/recents.ts
+++ src/lib/recents.ts
@@ -1,56 +1,35 @@
+import { writable, type Writable } from 'svelte/store';
 import type { NotesDB } from './database/notes';
 
-export default class Recents {
-	private maxSize: number = 10;
-	private notes: NotesDB[] = [];
+export const RECENT_NOTES: Writable<NotesDB[]> = writable([]);
+export const RECENT_NOTES_SIZE = 10;
 
-	/**
-	 * Get the notes in recents
-	 */
-	get getNotes() {
-		return this.notes;
-	}
-
-	/**
-	 * Set the notes in recents
-	 * @param notes NotesDB[] - notes to set
-	 */
-	set setNotes(notes: NotesDB[]) {
-		// check the duplicates
-		const uniqueNotes = notes.filter((note, index, self) => {
-			return self.findIndex((n) => n.id === note.id) === index;
-		});
-		this.notes = uniqueNotes;
-	}
-
-	/**
-	 * Function to add a note to the recents
-	 * @param note NotesDB - note to add
-	 * @returns void
-	 */
-	add(note: NotesDB) {
-		if (this.notes.find((n) => n.id === note.id)) return;
-		if (this.notes.length >= this.maxSize) {
-			// remove the first element
-			this.notes.shift();
-			this.notes.push(note);
-		} else {
-			this.notes.push(note);
+export function addNoteToRecents(note: NotesDB) {
+	RECENT_NOTES.update((notes) => {
+		if (notes.find((n) => n.id === note.id)) return notes;
+		if (notes.length >= RECENT_NOTES_SIZE) {
+			notes.shift();
+			return [...notes, note];
 		}
-	}
+		return [...notes, note];
+	});
+}
 
-	/**
-	 * Function to remove a note from the recents
-	 * @param note NotesDB - note to be removed
-	 */
-	remove(note: NotesDB) {
-		this.notes = this.notes.filter((n) => n.id !== note.id);
-	}
+export function removeNoteFromRecents(note: NotesDB) {
+	RECENT_NOTES.update((notes) => {
+		return notes.filter((n) => n.id !== note.id);
+	});
+}
 
-	/**
-	 * Function to clear all the notes from recents
-	 */
-	clear() {
-		this.notes = [];
-	}
+export function clearRecents() {
+	RECENT_NOTES.set([]);
+}
+
+export function getRecentNotesSize() {
+	let size = 0;
+	RECENT_NOTES.update((notes) => {
+		size = notes.length;
+		return notes;
+	});
+	return size;
 }
