1d4016db4e9a8fb957b7ff8ea32475416ecdc79b
Tsuzat <dev.tsuzat@gmail.com>
2025-01-15 23:08:10 +0530

    feat: Implement recent notes functionality with local storage integration

--- src/lib/recents.ts
+++ src/lib/recents.ts
@@ -1,8 +1,40 @@
 import { writable, type Writable } from 'svelte/store';
 import type { NotesDB } from './database/notes';
+import { load } from '@tauri-apps/plugin-store';
+import { NOTES } from './contants';
 
-export const RECENT_NOTES: Writable<NotesDB[]> = writable([]);
+export const RECENT_NOTES: Writable<NotesDB[]> = writable();
 export const RECENT_NOTES_SIZE = 10;
+
+RECENT_NOTES.subscribe(async (notes) => {
+	if (!notes) return;
+	const notesIds = notes.map((n) => n.id);
+	localStorage.setItem('recent-notes', JSON.stringify(notesIds));
+});
+
+/**
+ * Function to load recent notes from local storage
+ */
+export function loadRecents() {
+	try {
+		const rawData = localStorage.getItem('recent-notes') || '[]';
+		const notesIds: string[] = JSON.parse(rawData);
+		let notesInDB: NotesDB[] = [];
+		NOTES.update((notes) => {
+			notesInDB = notes;
+			return notes;
+		});
+		let recentNotes: NotesDB[] = [];
+		for (let noteId of notesIds) {
+			let note = notesInDB.find((n) => n.id === noteId);
+			if (note) recentNotes.push(note);
+		}
+		RECENT_NOTES.set(recentNotes);
+	} catch (error) {
+		console.error(error);
+		RECENT_NOTES.set([]);
+	}
+}
 
 export function addNoteToRecents(note: NotesDB) {
 	RECENT_NOTES.update((notes) => {
