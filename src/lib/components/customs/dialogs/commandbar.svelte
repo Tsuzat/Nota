0ad0c7329b557eb6dae1102e558548d216fcbca6
Tsuzat <dev.tsuzat@gmail.com>
2025-01-16 12:58:16 +0530

    feat(commandbar): enhance command item display with workspace information and update key binding

--- src/lib/components/customs/dialogs/commandbar.svelte
+++ src/lib/components/customs/dialogs/commandbar.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Command from '$lib/components/ui/command';
-	import { NOTES, OPEN_COMMAND_BAR } from '$lib/contants';
+	import { NOTES, OPEN_COMMAND_BAR, WORKSPACES } from '$lib/contants';
+	import type { NotesDB } from '$lib/database/notes';
+	import type { WorkSpaceDB } from '$lib/database/workspace';
 	import { Home } from 'lucide-svelte';
 
+	function getWorkSpace(note: NotesDB): WorkSpaceDB | undefined {
+		return $WORKSPACES.find((w) => w.id === note.workspace);
+	}
+
+	function handleGoto(path: string) {
+		goto(path);
+		OPEN_COMMAND_BAR.set(false);
+	}
+
 	function handleKeydown(e: KeyboardEvent) {
-		if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+		if (e.key === 'p' && (e.metaKey || e.ctrlKey)) {
 			e.preventDefault();
-			OPEN_COMMAND_BAR.set(!$OPEN_COMMAND_BAR);
+			OPEN_COMMAND_BAR.update((v) => !v);
 		}
 	}
 </script>
@@ -18,15 +30,26 @@
 	<Command.Input placeholder="Type a command or search..." />
 	<Command.List>
 		<Command.Empty>No results found.</Command.Empty>
-		<Command.Item value="home" keywords={['home', '/', 'homepage']} onclick={() => goto('/')}>
+		<Command.Item value="home" keywords={['home', '/', 'homepage']} onclick={() => handleGoto('/')}>
 			<Home />
 			<span>Home</span>
 		</Command.Item>
 		<Command.Group heading="All Notes">
 			{#each $NOTES as notes (notes.id)}
-				<Command.Item value={notes.id} keywords={[notes.name]} onclick={() => goto(`/${notes.id}`)}>
-					<div>{notes.icon}</div>
-					<span>{notes.name}</span>
+				{@const workspace = getWorkSpace(notes)}
+				<Command.Item
+					value={notes.id}
+					class="flex items-center w-full"
+					keywords={[notes.name, workspace?.name || '']}
+					onclick={() => handleGoto(`/${notes.id}`)}
+				>
+					<div class="text-ellipsis w-[50%]">{notes.icon} {notes.name}</div>
+					{#if workspace}
+						<div class="text-xs text-muted-foreground text-ellipsis">
+							{workspace.icon}
+							{workspace.name}
+						</div>
+					{/if}
 				</Command.Item>
 			{/each}
 		</Command.Group>
