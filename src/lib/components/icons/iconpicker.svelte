bcff89f2fb8c0247746e8b7b184eb2c6db2c2e61
Tsuzat <dev.tsuzat@gmail.com>
2025-01-23 14:19:56 +0530

    feat: integrate SvgIconPicker into iconpicker component for enhanced icon selection

--- src/lib/components/icons/iconpicker.svelte
+++ src/lib/components/icons/iconpicker.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import * as Popover from '$lib/components/ui/popover';
 	import * as Tabs from '$lib/components/ui/tabs';
+	import { fly } from 'svelte/transition';
 	import { buttonVariants } from '../ui/button';
 	import Emojipicker from './emojipicker.svelte';
+	import SvgIconPicker from './svg-icon-picker.svelte';
 	interface Props {
 		onSelect: (icon: string) => void;
 		side?: 'bottom' | 'top' | 'left' | 'right';
@@ -18,16 +20,16 @@
 	<Popover.Content class="size-fit p-0 shadow-2xl" {side}>
 		<Tabs.Root value="emojis" class="w-fit">
 			<Tabs.List class="bg-transparent">
-				<!-- <Tabs.Trigger value="icons" class="data-[state=active]:underline font-bold"
+				<Tabs.Trigger value="icons" class="data-[state=active]:underline font-bold"
 					>Icons</Tabs.Trigger
-				> -->
+				>
 				<Tabs.Trigger value="emojis" class="data-[state=active]:underline font-bold"
 					>Emojis</Tabs.Trigger
 				>
 			</Tabs.List>
-			<!-- <Tabs.Content value="icons">
-				<LucideIcons name={'FolderIcon'} {onSelect} />
-			</Tabs.Content> -->
+			<Tabs.Content value="icons">
+				<SvgIconPicker />
+			</Tabs.Content>
 			<Tabs.Content value="emojis">
 				<Emojipicker {onSelect} />
 			</Tabs.Content>
