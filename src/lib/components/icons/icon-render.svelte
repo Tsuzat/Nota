8cdccce4375521d95f9cc43b55c0fc8297cd73b0
Tsuzat <dev.tsuzat@gmail.com>
2025-01-23 17:21:09 +0530

    feat: implement dynamic icon rendering based on icon type (emoji, SVG, or image)

--- src/lib/components/icons/icon-render.svelte
+++ src/lib/components/icons/icon-render.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
+	import { cn } from '$lib/utils';
+	import SvgIcon from './svg-icon.svelte';
+	import { getIconTypeAndData } from './utils';
+
 	interface Props {
 		icon: string;
 		class?: string;
 	}
 	let { icon, class: className = '' }: Props = $props();
+
+	const iconType = $derived.by(() => getIconTypeAndData(icon));
 </script>
 
-<span class={className}>{icon}</span>
+{#if iconType.type === 'emoji'}
+	<span class={className}>
+		{iconType.icon}
+	</span>
+{:else if iconType.type === 'svg'}
+	<SvgIcon body={iconType.icon} class={cn(className, 'size-6')} />
+{:else}
+	<img src={iconType.icon} alt="icon" class={className} />
+{/if}
