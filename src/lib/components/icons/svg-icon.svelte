2b7eb9e2a3666beee9d1bf17fedf2019584fe40c
Tsuzat <dev.tsuzat@gmail.com>
2025-01-23 14:19:45 +0530

    feat: add SvgIcon component for rendering SVG content with customizable props

---
+++ src/lib/components/icons/svg-icon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	interface Props {
+		body: string;
+		[key: string]: any;
+	}
+
+	let { body, ...rest }: Props = $props();
+</script>
+
+<svg role="img" viewBox="0 0 24 24" {...rest}>
+	{@html body}
+</svg>
