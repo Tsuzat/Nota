4dc9462b1c507adad1d3ce7fe5c42a584176c7a4
Tsuzat <dev.tsuzat@gmail.com>
2025-01-28 21:35:36 +0530

    feat: replace TeamSwitcher with UserProfile in sidebar and remove unused TeamSwitcher component

--- src/lib/components/customs/sidebar/app-sidebar.svelte
+++ src/lib/components/customs/sidebar/app-sidebar.svelte
@@ -1,26 +1,7 @@
 <script lang="ts" module>
-	import AudioWaveform from 'lucide-svelte/icons/audio-waveform';
-	import Command from 'lucide-svelte/icons/command';
 	import House from 'lucide-svelte/icons/house';
 	import Search from 'lucide-svelte/icons/search';
 	const data = {
-		teams: [
-			{
-				name: 'Acme Inc',
-				logo: Command,
-				plan: 'Enterprise'
-			},
-			{
-				name: 'Acme Corp.',
-				logo: AudioWaveform,
-				plan: 'Startup'
-			},
-			{
-				name: 'Evil Corp.',
-				logo: Command,
-				plan: 'Free'
-			}
-		],
 		navMain: [
 			{
 				title: 'Home',
@@ -45,17 +26,17 @@
 	import NavMain from '$lib/components/customs/sidebar/nav-main.svelte';
 	import NavSecondary from '$lib/components/customs/sidebar/nav-secondary.svelte';
 	import NavWorkspaces from '$lib/components/customs/sidebar/nav-workspaces.svelte';
-	import TeamSwitcher from '$lib/components/customs/sidebar/team-switcher.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import type { ComponentProps } from 'svelte';
 	import { OPEN_COMMAND_BAR } from '$lib/contants';
+	import UserProfile from './user-profile.svelte';
 
 	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
 </script>
 
 <Sidebar.Root bind:ref class="border-r-0" {...restProps}>
 	<Sidebar.Header>
-		<TeamSwitcher teams={data.teams} />
+		<UserProfile />
 		<NavMain items={data.navMain} />
 	</Sidebar.Header>
 	<Sidebar.Content>
---
+++ src/lib/components/customs/sidebar/user-profile.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import Themetoggler from '../themetoggler.svelte';
+	import { SHOW_DECORATION } from '$lib/contants';
+	import IconRender from '$lib/components/icons/icon-render.svelte';
+	import { USER_ICON, USER_NAME } from '$lib/app_settings';
+	import { Button } from '$lib/components/ui/button';
+	import { APP_MENU } from '$lib/app_menu';
+
+	async function onClickIcon() {
+		await APP_MENU.popup();
+	}
+</script>
+
+<Sidebar.Menu>
+	<Sidebar.MenuItem
+		{...$SHOW_DECORATION ? {} : { 'data-tauri-drag-region': '' }}
+		class="flex items-center justify-between "
+	>
+		<div class="flex items-center gap-2 w-full mr-8">
+			{#if $SHOW_DECORATION}
+				<IconRender icon={$USER_ICON} />
+			{:else}
+				<Button variant="ghost" size="icon" class="size-fit p-1" onclick={onClickIcon}>
+					<IconRender icon={$USER_ICON} />
+				</Button>
+			{/if}
+			<span class="text-ellipsis font-bold">{$USER_NAME}</span>
+		</div>
+		<Themetoggler />
+	</Sidebar.MenuItem>
+</Sidebar.Menu>
