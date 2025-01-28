40cafedd43d7bd5bd4d1bd5e0cc3e8ccf7eeedea
Tsuzat <dev.tsuzat@gmail.com>
2025-01-28 23:26:25 +0530

    feat: add help submenu with update check and about information in app menu

--- src/lib/app_menu.ts
+++ src/lib/app_menu.ts
@@ -1,10 +1,43 @@
-import { Menu } from '@tauri-apps/api/menu';
+import { Menu, MenuItem, Submenu } from '@tauri-apps/api/menu';
+import { checkUpdate } from './updater';
+import { message } from '@tauri-apps/plugin-dialog';
+import { getVersion } from '@tauri-apps/api/app';
 
 export let APP_MENU: Menu;
 
+async function loadHelpMenu(): Promise<Submenu> {
+	let checkUpdateMenuItem = await MenuItem.new({
+		text: 'Check for Updates',
+		action: () => {
+			checkUpdate();
+		}
+	});
+
+	let aboutAppMenuItem = await MenuItem.new({
+		text: 'About Nota',
+		action: async () => {
+			message(
+				`Name: Nota\nVersion: ${await getVersion()}\nAuthor: Alok "Tsuzat" Singh\nWebsite: https://github.com/Tsuzat/Nota`,
+				{
+					title: 'About Nota',
+					kind: 'info'
+				}
+			);
+		}
+	});
+
+	let items: MenuItem[] = [checkUpdateMenuItem, aboutAppMenuItem];
+
+	return await Submenu.new({ text: 'Help', items });
+}
+
 async function initializeAppMenu() {
-	APP_MENU = await Menu.default();
-	APP_MENU.setAsAppMenu();
+	const menu = await Menu.default();
+	const menuItems = await menu.items();
+	menuItems.pop();
+	menuItems.push(await loadHelpMenu());
+	APP_MENU = await Menu.new({ items: menuItems });
+	await APP_MENU.setAsAppMenu();
 }
 
 export default initializeAppMenu;
