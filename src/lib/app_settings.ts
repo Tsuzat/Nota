2c7c995873b7493140729cc48cd86b2abd24e03c
Tsuzat <dev.tsuzat@gmail.com>
2025-01-29 17:00:29 +0530

    feat: add global writable variables for user settings and sidebar state management

--- src/lib/app_settings.ts
+++ src/lib/app_settings.ts
@@ -1,21 +1,64 @@
-import { error } from '@tauri-apps/plugin-log';
-import { load, type Store } from '@tauri-apps/plugin-store';
-import { toast } from 'svelte-sonner';
-import { writable } from 'svelte/store';
+import { writable, type Writable } from 'svelte/store';
 import { APPWINDOW, OS } from './contants';
+import { browser } from '$app/environment';
 
-let APP_SETTINGS: Store;
-
-export async function loadAppSettings() {
-	try {
-		APP_SETTINGS = await load('app_settings.json', { autoSave: true });
-	} catch (e: any) {
-		console.error(e);
-		error(e.toString());
-		toast.error('Error loading app settings. Populating with defaults.');
+/**
+ * Global Variable for keeping track of the user name
+ */
+export const USER_NAME: Writable<string> = writable(
+	localStorage.getItem('user_name') || 'Unknown User'
+);
+USER_NAME.subscribe(async (value) => {
+	if (browser) {
+		localStorage.setItem('user_name', value);
 	}
-}
+});
 
-export const USER_NAME = writable('Unknown User');
-export const USER_ICON = writable('emoji:🙍');
-export const SIDEBAR_OPEN = writable(true);
+/**
+ * Global Variable for keeping track of the user icon
+ */
+export const USER_ICON: Writable<string> = writable(
+	localStorage.getItem('user_icon') || 'emoji:😂'
+);
+USER_ICON.subscribe(async (value) => {
+	if (browser) {
+		localStorage.setItem('user_icon', value);
+	}
+});
+
+/**
+ * Global Variable for keeping track of the sidebar open state
+ */
+export const SIDEBAR_OPEN: Writable<boolean> = writable(
+	(localStorage.getItem('sidebar_open') ?? 'true') === 'true'
+);
+SIDEBAR_OPEN.subscribe(async (value) => {
+	if (browser) {
+		localStorage.setItem('sidebar_open', value ? 'true' : 'false');
+	}
+});
+
+/**
+ * Global Variable for toggling window decoration
+ * @Platform specific - Supports `Windows` only
+ * */
+export const SHOW_DECORATION: Writable<boolean> = writable(
+	(localStorage.getItem('show_decorations') ?? 'true') === 'true'
+);
+SHOW_DECORATION.subscribe(async (value) => {
+	if (OS !== 'windows') return;
+	await APPWINDOW.setDecorations(value);
+	if (browser) {
+		localStorage.setItem('show_decorations', value ? 'true' : 'false');
+	}
+});
+
+/** Global Variable for controlling check update on start */
+export const CHECK_UPDATE_ON_START: Writable<boolean> = writable(
+	(localStorage.getItem('check_update_on_start') ?? 'true') === 'true'
+);
+CHECK_UPDATE_ON_START.subscribe(async (value) => {
+	if (browser) {
+		localStorage.setItem('check_update_on_start', value ? 'true' : 'false');
+	}
+});
