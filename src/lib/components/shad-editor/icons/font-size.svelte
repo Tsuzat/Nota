3b597747871685a5a1add62d2f4f3853812c2115
Tsuzat <dev.tsuzat@gmail.com>
2025-02-10 20:14:42 +0530

    feat: add font size dropdown component for Tiptap editor

---
+++ src/lib/components/shad-editor/icons/font-size.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { buttonVariants } from '$lib/components/ui/button';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import type { Editor } from '@tiptap/core';
+	import { ChevronDown } from 'lucide-svelte';
+
+	interface Props {
+		editor: Editor;
+	}
+
+	let { editor }: Props = $props();
+
+	const FONT_SIZE = [
+		{ label: 'Smaller', value: '12px' },
+		{ label: 'Small', value: '14px' },
+		{ label: 'Medium', value: '' },
+		{ label: 'Large', value: '18px' },
+		{ label: 'Extra Large', value: '24px' }
+	];
+
+	let currentSize = $derived.by(() => editor.getAttributes('textStyle').fontSize || '');
+
+	const currentLabel = $derived.by(() => {
+		const l = FONT_SIZE.find((f) => f.value === currentSize);
+		if (l) return l.label.split(' ')[0];
+		return 'Medium';
+	});
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger
+		class={buttonVariants({ variant: 'ghost', size: 'sm', class: 'h-8 w-fit p-0 px-2' })}
+	>
+		<span>{currentLabel}</span>
+		<ChevronDown class="!size-4" />
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content class="w-fit h-fit">
+		{#each FONT_SIZE as fontSize}
+			<DropdownMenu.Item
+				onclick={() => {
+					editor.chain().focus().setFontSize(fontSize.value).run();
+				}}
+				style={`font-size: ${fontSize.value}`}>{fontSize.label}</DropdownMenu.Item
+			>
+		{/each}
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
