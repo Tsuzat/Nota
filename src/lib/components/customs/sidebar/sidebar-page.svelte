31fdba1ac0b60c0520acb0c3fc426f91bad87be9
Tsuzat <dev.tsuzat@gmail.com>
2025-02-10 20:14:50 +0530

    style: adjust sidebar page overflow handling

--- src/lib/components/customs/sidebar/sidebar-page.svelte
+++ src/lib/components/customs/sidebar/sidebar-page.svelte
@@ -4,7 +4,7 @@
 	let { children } = $props();
 </script>
 
-<Sidebar.Provider class="h-[100dvh] w-[100dvw] overflow-hidden overflow-x-scroll">
+<Sidebar.Provider class="overflow-y-hidden">
 	<AppSidebar />
 	<Sidebar.Inset>
 		{@render children()}
