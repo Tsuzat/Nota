52a9a3b458512b6cb9bf0bda9a407b82cc351193
Tsuzat <dev.tsuzat@gmail.com>
2025-02-11 11:40:36 +0530

    feat: improve link component with URL validation and enhanced UI

--- src/lib/components/shad-editor/icons/link.svelte
+++ src/lib/components/shad-editor/icons/link.svelte
@@ -1,30 +1,38 @@
 <script lang="ts">
-	import { Link } from 'lucide-svelte';
+	import { Check, Link } from 'lucide-svelte';
 	import { ChevronDown } from 'lucide-svelte';
 	import { X } from 'lucide-svelte';
 	import { type Editor } from '@tiptap/core';
 	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
-	import { cn } from '$lib/utils.js';
+	import { cn, validateURL } from '$lib/utils.js';
 	import * as Popover from '$lib/components/ui/popover/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
 
 	let { editor }: { editor: Editor } = $props();
 
-	function setLink(url: string) {
-		if (url.trim() === '') {
+	let link = $state(editor.getAttributes('link').href);
+
+	let isLinkValid = $state(true);
+
+	function setLink() {
+		if (link.trim() === '') {
 			editor.chain().focus().extendMarkRange('link').unsetLink().run();
 			return;
 		}
-		editor.chain().focus().extendMarkRange('link').setLink({ href: url }).run();
+		editor.chain().focus().extendMarkRange('link').setLink({ href: link }).run();
 	}
+
+	$effect(() => {
+		isLinkValid = validateURL(link);
+	});
 </script>
 
-<Tooltip.Provider delayDuration={100}>
-	<Tooltip.Root>
-		<Tooltip.Trigger>
-			<Popover.Root>
-				<Popover.Trigger>
+<Popover.Root>
+	<Popover.Trigger>
+		<Tooltip.Provider delayDuration={100}>
+			<Tooltip.Root>
+				<Tooltip.Trigger>
 					<Button
 						variant="ghost"
 						size="sm"
@@ -33,40 +41,43 @@
 						<Link />
 						<ChevronDown class="!size-3 text-muted-foreground" />
 					</Button>
-				</Popover.Trigger>
-				<Popover.Content class="bg-popover shadow-lg *:my-2">
-					<div class="flex items-center justify-between">
-						<h1 class="text-xl font-bold">Link</h1>
-						<Popover.Close>
-							<X class="size-4 text-muted-foreground" />
-						</Popover.Close>
-					</div>
-					<p>Insert or remove link from selected text.</p>
-					<Input
-						placeholder="Enter link to attach.."
-						value={editor?.getAttributes('link').href}
-						onchange={(e) => {
-							//@ts-ignore
-							if (e !== null && e.target !== null) setLink(e.target.value);
-						}}
-						class="w-full"
-					/>
-					<div class="flex items-center justify-end gap-2">
-						<Button size="sm" onclick={() => {}}>
-							<Popover.Close>Insert</Popover.Close>
-						</Button>
-						<Button
-							variant="destructive"
-							onclick={() => {
-								editor.chain().focus().extendMarkRange('link').unsetLink().run();
-							}}>Remove</Button
-						>
-					</div>
-				</Popover.Content>
-			</Popover.Root>
-		</Tooltip.Trigger>
-		<Tooltip.Content avoidCollisions class="bg-background text-foreground border font-medium p-2">
-			<p>Add or Remove Link</p>
-		</Tooltip.Content>
-	</Tooltip.Root>
-</Tooltip.Provider>
+				</Tooltip.Trigger>
+				<Tooltip.Content
+					avoidCollisions
+					class="bg-background text-foreground border font-medium p-2"
+				>
+					<p>Add or Remove Link</p>
+				</Tooltip.Content>
+			</Tooltip.Root>
+		</Tooltip.Provider>
+	</Popover.Trigger>
+	<Popover.Content
+		class="bg-popover shadow-lg w-80 h-fit flex items-center gap-2 p-2 relative"
+		portalProps={{ disabled: true, to: undefined }}
+		side="right"
+	>
+		<Input
+			placeholder="Enter link to attach.."
+			value={editor.getAttributes('link').href}
+			type="url"
+			class={cn(isLinkValid && 'border-green-500', !isLinkValid && 'border-red-500')}
+			oninput={(e) => {
+				//@ts-ignore
+				link = e.target.value;
+			}}
+		/>
+		<Button variant="ghost" class="size-8" onclick={setLink} title="Insert Link">
+			<Check />
+		</Button>
+		<Button
+			variant="ghost"
+			class="size-8"
+			onclick={() => {
+				editor.chain().focus().extendMarkRange('link').unsetLink().run();
+			}}
+			title="Remove Link"
+		>
+			<X />
+		</Button>
+	</Popover.Content>
+</Popover.Root>
