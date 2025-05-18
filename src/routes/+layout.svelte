3c7e1bff3bcadb5bf76b7b9bc5c89ccce121dfbc
Tsuzat <dev.tsuzat@gmail.com>
2025-05-18 16:18:21 +0530

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	import '@fontsource-variable/open-sans';
+	import { ModeWatcher } from 'mode-watcher';
+
+	let { children } = $props();
+</script>
+
+<ModeWatcher />
+<main class="h-screen w-screen">
+	{@render children()}
+</main>
