804ef8ed640bda8c70c34429a92509069559f56b
Tsuzat <dev.tsuzat@gmail.com>
2025-05-18 17:42:20 +0530

    feat: add greeting functionality and confirmation dialog to the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
 	import { invoke } from '@tauri-apps/api/core';
+	import { confirm } from '@tauri-apps/plugin-dialog';
 
 	let name = $state('');
 	let greetMsg = $state('');
+
+	async function askMe() {
+		const hello = confirm('Is this windows is visible to you ??');
+	}
 
 	async function greet(event: Event) {
 		event.preventDefault();
@@ -10,3 +17,15 @@
 		greetMsg = await invoke('greet', { name });
 	}
 </script>
+
+<div class="flex flex-col h-full gap-8 items-center justify-center">
+	<h1>Hello World!</h1>
+	<form class="flex items-center justify-center gap-2" onsubmit={greet}>
+		<Input bind:value={name} />
+		<Button type="submit" class="rounded-full">Submit</Button>
+	</form>
+	{#if greetMsg.trim() !== ''}
+		<p>{greetMsg}</p>
+	{/if}
+	<Button onclick={askMe}>Show Dialog</Button>
+</div>
