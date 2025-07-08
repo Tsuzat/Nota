af758723d98694d5347c6295bd2482088aa0dc12
Tsuzat <dev.tsuzat@gmail.com>
2025-07-08 23:47:18 +0530

    feat: add initial layout and page components with Tauri detection toast notifications

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
-	import '@fontsource-variable/dm-sans';
 	import '../app.css';
+	import '@fontsource-variable/dm-sans';
 	import { ModeWatcher } from 'mode-watcher';
+	import { onMount } from 'svelte';
+	import { Toaster } from '$lib/components/ui/sonner';
+	import { toast } from 'svelte-sonner';
+	import { ISTAURI } from '$lib/utils';
 
 	let { children } = $props();
+
+	onMount(() => {
+		if (ISTAURI) {
+			toast.info("Inside Tauri's window");
+		} else {
+			toast.info('Inside the browser');
+		}
+	});
 </script>
+
+<Toaster richColors closeButton position="top-right" />
 
 <ModeWatcher />
 
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<main class="flex h-screen w-screen flex-col items-center justify-center">
+	<h1>We'll Create you later</h1>
+</main>
