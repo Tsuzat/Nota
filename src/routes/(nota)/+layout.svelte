a574b8967e04c5325abb08214f58f26bd80cfbd3
Tsuzat <dev.tsuzat@gmail.com>
2025-07-10 21:59:59 +0530

    refactor(nota): remove unused files and enhance layout with local data handling
    Remove unused route files and restructure layout component to handle local data initialization and workspace management
    Add button to toggle icon picker for better user interaction

--- src/routes/(nota)/+layout.svelte
+++ src/routes/(nota)/+layout.svelte
@@ -1,11 +1,55 @@
 <script lang="ts">
 	import AppSidebar from '$lib/components/custom/side-bar/app-sidebar.svelte';
 	import NavActions from '$lib/components/custom/side-bar/nav-actions.svelte';
-	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { getLocalNotes, setLocalNotes } from '$lib/local/notes.svelte';
+	import { getLocalUserWorkspaces, setLocalUserWorkspaces } from '$lib/local/userworkspaces.svelte';
+	import { getLocalWorkspaces, setLocalWorkspaces } from '$lib/local/workspaces.svelte';
+	import { ISTAURI } from '$lib/utils';
+	import { onMount } from 'svelte';
+	import { toast } from 'svelte-sonner';
+	import { BaseDirectory, readDir } from '@tauri-apps/plugin-fs';
+	import IconPicker from '$lib/components/icons/icon-picker.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+
+	setLocalUserWorkspaces();
+	setLocalWorkspaces();
+	setLocalNotes();
+
+	onMount(async () => {
+		if (ISTAURI) {
+			const d = await readDir('', { baseDir: BaseDirectory.AppConfig });
+			console.log(d.map((d) => d.name));
+
+			try {
+				const localUserWorkspaces = getLocalUserWorkspaces();
+				const localWorkspaces = getLocalWorkspaces();
+				const localNotes = getLocalNotes();
+
+				await localUserWorkspaces.fetchUserWorkspaces();
+				const curLocUsrWrkspc = localUserWorkspaces.getCurrentUserWorkspace();
+				if (curLocUsrWrkspc === undefined) {
+					toast.warning('No user workspace found. Creating a new one');
+					await localUserWorkspaces.createUserWorkspace('My Workspace', 'User');
+				}
+				if (curLocUsrWrkspc) {
+					await localWorkspaces.fetchWorkspaces(curLocUsrWrkspc.id);
+					await localNotes.fetchNotes(curLocUsrWrkspc.id);
+				}
+			} catch (e) {
+				toast.error('Something went wrong when loading the local data');
+				console.error(e);
+			}
+		}
+	});
 
 	const { children } = $props();
+
+	let open = $state(false);
+	function toggleOpen() {
+		open = !open;
+	}
 </script>
 
 <Sidebar.Provider>
@@ -15,6 +59,10 @@
 			<div class="flex flex-1 items-center gap-2 px-3">
 				<Sidebar.Trigger />
 				<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+				<Button onclick={toggleOpen}>
+					<IconPicker bind:open />
+					open
+				</Button>
 				<h3>My Notes</h3>
 			</div>
 			<div class="ml-auto px-3">
