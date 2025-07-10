72e8f05ad5913d76ea63cc750069b0ae26c89bc9
Tsuzat <dev.tsuzat@gmail.com>
2025-07-10 22:00:31 +0530

    feat(home): add editor page with localStorage persistence
    Implement a new home page with a rich text editor that persists content to localStorage. Includes bubble menu and drag handle components for enhanced editing experience.

---
+++ src/routes/(nota)/home/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { EdraBubbleMenu, EdraDragHandleExtended, EdraEditor } from '$lib/components/edra/shadcn';
+	import type { Content, Editor } from '@tiptap/core';
+	let content = $state<Content>();
+	let editor = $state<Editor>();
+
+	if (browser) {
+		const raw = localStorage.getItem('content');
+		if (raw) {
+			content = JSON.parse(raw);
+		} else {
+			content = {};
+		}
+	}
+
+	function onUpdate() {
+		content = editor?.getJSON();
+		localStorage.setItem('content', JSON.stringify(content));
+	}
+</script>
+
+{#if editor && !editor?.isDestroyed}
+	<EdraDragHandleExtended {editor} />
+	<EdraBubbleMenu {editor} />
+{/if}
+<EdraEditor bind:editor {content} class="size-full !p-8" {onUpdate} />
