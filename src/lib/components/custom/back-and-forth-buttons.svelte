bd72d31b7e925e821bc8e1cca4a6799cd1bb76ed
Tsuzat <dev.tsuzat@gmail.com>
2025-07-14 22:04:11 +0530

    Add window controls and navigation buttons

---
+++ src/lib/components/custom/back-and-forth-buttons.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { ArrowLeft, ArrowRight } from '@lucide/svelte';
+	import { Button } from '$lib/components/ui/button';
+	import SimpleTooltip from './simple-tooltip.svelte';
+
+	async function gotoPrev() {
+		window.history.back();
+	}
+
+	async function gotoNext() {
+		window.history.forward();
+	}
+</script>
+
+<div class="flex items-center">
+	<SimpleTooltip content="Go Back">
+		<Button variant="ghost" class="size-7" onclick={gotoPrev}>
+			<ArrowLeft />
+		</Button>
+	</SimpleTooltip>
+	<SimpleTooltip content="Go Next">
+		<Button variant="ghost" class="size-7" onclick={gotoNext}>
+			<ArrowRight />
+		</Button>
+	</SimpleTooltip>
+</div>
