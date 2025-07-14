f95289a9dfc2614d96474b550c2dee0ab1e1ea85
Tsuzat <dev.tsuzat@gmail.com>
2025-07-14 22:05:33 +0530

    Add theme toggle and fix navigation links

--- src/lib/components/custom/side-bar/app-sidebar.svelte
+++ src/lib/components/custom/side-bar/app-sidebar.svelte
@@ -19,7 +19,7 @@
 			},
 			{
 				title: 'Home',
-				url: '#',
+				url: '/home',
 				icon: HouseIcon,
 				isActive: true
 			}
@@ -52,13 +52,32 @@
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import type { ComponentProps } from 'svelte';
 	import NavWorkspacesLocal from './nav-workspaces-local.svelte';
-	import { ISTAURI } from '$lib/utils';
+	import { ISMACOS, ISTAURI } from '$lib/utils';
+	import { Button } from '$lib/components/ui/button';
+	import { MoonStar, Sun } from '@lucide/svelte';
+	import { toggleMode } from 'mode-watcher';
+	import SimpleTooltip from '../simple-tooltip.svelte';
 
 	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
 </script>
 
 <Sidebar.Root bind:ref class="border-r-0" {...restProps}>
 	<Sidebar.Header>
+		<div data-tauri-drag-region class="flex h-8 items-center justify-between">
+			{#if ISMACOS}
+				<div></div>
+			{:else}
+				<Button variant="ghost" size="icon" class="z-10">
+					<img src="/favicon.png" alt="AppLogo" class="aspect-square size-6" />
+				</Button>
+			{/if}
+			<SimpleTooltip content="Toggle Mode">
+				<Button variant="ghost" size="icon" class="z-10 !size-7" onclick={toggleMode}>
+					<Sun class="block dark:hidden" />
+					<MoonStar class="hidden dark:block" />
+				</Button>
+			</SimpleTooltip>
+		</div>
 		<TeamSwitcher />
 		<NavMain items={data.navMain} />
 	</Sidebar.Header>
