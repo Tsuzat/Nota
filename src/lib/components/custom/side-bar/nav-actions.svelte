4ab3e137a76835c75294c3a24d231202441f2780
Tsuzat <dev.tsuzat@gmail.com>
2025-07-18 22:18:36 +0530

    refactor(side-bar): simplify nav actions structure and improve settings organization
    - Restructure navigation actions from nested arrays to flat structure
    - Move settings options into a collapsible submenu
    - Add trash-related actions in a separate group

--- src/lib/components/custom/side-bar/nav-actions.svelte
+++ src/lib/components/custom/side-bar/nav-actions.svelte
@@ -1,79 +1,33 @@
 <script lang="ts" module>
 	import ArrowDownIcon from '@lucide/svelte/icons/arrow-down';
 	import ArrowUpIcon from '@lucide/svelte/icons/arrow-up';
-	import BellIcon from '@lucide/svelte/icons/bell';
-	import ChartLineIcon from '@lucide/svelte/icons/chart-line';
 	import CopyIcon from '@lucide/svelte/icons/copy';
-	import CornerUpLeftIcon from '@lucide/svelte/icons/corner-up-left';
-	import CornerUpRightIcon from '@lucide/svelte/icons/corner-up-right';
-	import FileTextIcon from '@lucide/svelte/icons/file-text';
 	import GalleryVerticalEndIcon from '@lucide/svelte/icons/gallery-vertical-end';
 	import LinkIcon from '@lucide/svelte/icons/link';
 	import Settings2Icon from '@lucide/svelte/icons/settings-2';
-	import TrashIcon from '@lucide/svelte/icons/trash';
 	import Trash2Icon from '@lucide/svelte/icons/trash-2';
 
 	const data = [
-		[
-			{
-				label: 'Note Settings',
-				icon: Settings2Icon
-			},
-			{
-				label: 'Turn into wiki',
-				icon: FileTextIcon
-			}
-		],
-		[
-			{
-				label: 'Copy Link',
-				icon: LinkIcon
-			},
-			{
-				label: 'Duplicate',
-				icon: CopyIcon
-			},
-			{
-				label: 'Move to',
-				icon: CornerUpRightIcon
-			},
-			{
-				label: 'Move to Trash',
-				icon: Trash2Icon
-			}
-		],
-		[
-			{
-				label: 'Undo',
-				icon: CornerUpLeftIcon
-			},
-			{
-				label: 'View analytics',
-				icon: ChartLineIcon
-			},
-			{
-				label: 'Version History',
-				icon: GalleryVerticalEndIcon
-			},
-			{
-				label: 'Show delete pages',
-				icon: TrashIcon
-			},
-			{
-				label: 'Notifications',
-				icon: BellIcon
-			}
-		],
-		[
-			{
-				label: 'Import',
-				icon: ArrowUpIcon
-			},
-			{
-				label: 'Export',
-				icon: ArrowDownIcon
-			}
-		]
+		{
+			label: 'Copy Link',
+			icon: LinkIcon
+		},
+		{
+			label: 'Duplicate',
+			icon: CopyIcon
+		},
+		{
+			label: 'Version History',
+			icon: GalleryVerticalEndIcon
+		},
+		{
+			label: 'Import',
+			icon: ArrowUpIcon
+		},
+		{
+			label: 'Export',
+			icon: ArrowDownIcon
+		}
 	];
 </script>
 
@@ -137,44 +91,71 @@
 		</Dropdown.Trigger>
 		<Dropdown.Content class="bg-popover h-full w-fit overflow-auto" align="end">
 			<Dropdown.Group>
-				<Dropdown.GroupHeading>Notes Page Settings</Dropdown.GroupHeading>
-				<Dropdown.CheckboxItem
-					checked={settings.locked}
-					onclick={() => (settings = { ...settings, locked: !settings.locked })}
-				>
-					<Lock />
-					{settings.locked ? 'Lock' : 'Unlock'}
-				</Dropdown.CheckboxItem>
-				<Dropdown.CheckboxItem
-					checked={settings.showtoolbar}
-					onclick={() => (settings = { ...settings, showtoolbar: !settings.showtoolbar })}
-				>
-					<PenTool />
-					Toolbar
-				</Dropdown.CheckboxItem>
-				<Dropdown.CheckboxItem
-					checked={settings.spellcheck}
-					onclick={() => (settings = { ...settings, spellcheck: !settings.spellcheck })}
-				>
-					<SpellCheck />
-					Spell Check
-				</Dropdown.CheckboxItem>
-				<Dropdown.CheckboxItem
-					checked={settings.showbubblemenu}
-					onclick={() => (settings = { ...settings, showbubblemenu: !settings.showbubblemenu })}
-				>
-					<Bubbles />
-					Bubble Menu
-				</Dropdown.CheckboxItem>
-				<Dropdown.CheckboxItem
-					checked={settings.compressmedia}
-					onclick={() => (settings = { ...settings, compressmedia: !settings.compressmedia })}
-				>
-					<Film />
-					Compress Media
-				</Dropdown.CheckboxItem>
+				<Dropdown.Sub>
+					<Dropdown.SubTrigger>
+						<Settings2Icon />
+						Settings
+					</Dropdown.SubTrigger>
+					<Dropdown.SubContent>
+						<Dropdown.CheckboxItem
+							checked={settings.locked}
+							onclick={() => (settings = { ...settings, locked: !settings.locked })}
+						>
+							<Lock />
+							{settings.locked ? 'Unlock' : 'Lock'}
+						</Dropdown.CheckboxItem>
+						<Dropdown.CheckboxItem
+							checked={settings.showtoolbar}
+							onclick={() => (settings = { ...settings, showtoolbar: !settings.showtoolbar })}
+						>
+							<PenTool />
+							Toolbar
+						</Dropdown.CheckboxItem>
+						<Dropdown.CheckboxItem
+							checked={settings.spellcheck}
+							onclick={() => (settings = { ...settings, spellcheck: !settings.spellcheck })}
+						>
+							<SpellCheck />
+							Spell Check
+						</Dropdown.CheckboxItem>
+						<Dropdown.CheckboxItem
+							checked={settings.showbubblemenu}
+							onclick={() => (settings = { ...settings, showbubblemenu: !settings.showbubblemenu })}
+						>
+							<Bubbles />
+							Bubble Menu
+						</Dropdown.CheckboxItem>
+						<Dropdown.CheckboxItem
+							checked={settings.compressmedia}
+							onclick={() => (settings = { ...settings, compressmedia: !settings.compressmedia })}
+						>
+							<Film />
+							Compress Media
+						</Dropdown.CheckboxItem>
+					</Dropdown.SubContent>
+				</Dropdown.Sub>
 			</Dropdown.Group>
 			<Dropdown.Separator />
+			<Dropdown.Group>
+				{#each data as d}
+					{@const Icon = d.icon}
+					<Dropdown.Item>
+						<Icon />
+						{d.label}
+					</Dropdown.Item>
+				{/each}
+			</Dropdown.Group>
+			<Dropdown.Separator />
+			<Dropdown.Group>
+				<Dropdown.Item>
+					<Trash2Icon />
+					<span>Move to Trash</span>
+				</Dropdown.Item>
+				<Dropdown.Item variant="destructive">
+					<Trash2Icon />
+					<span>Delete Note</span>
+				</Dropdown.Item>
+			</Dropdown.Group>
 		</Dropdown.Content>
 	</Dropdown.Root>
 </div>
