8e9cdc40971e347ae1ba022a277eee979fab5d68
Tsuzat <dev.tsuzat@gmail.com>
2025-07-22 09:22:22 +0530

    feat(updater): add auto-update functionality with progress tracking
    Implement update checking and installation with toast notifications

---
+++ src/lib/updater.ts
@@ -0,0 +1,68 @@
+import { check, Update } from '@tauri-apps/plugin-updater';
+import { relaunch } from '@tauri-apps/plugin-process';
+import { toast } from 'svelte-sonner';
+import { getVersion } from '@tauri-apps/api/app';
+
+/**
+ * Function to check for updates
+ * @param isAuto boolean - weather the check is triggered by app or user, if true then it's triggered by app
+ */
+export async function checkUpdate(isAuto: boolean = false) {
+	const id = toast.loading('Checking for updates...');
+	const update = await check();
+	if (update !== null) {
+		toast.info(`New version ${update.version} is available.`, {
+			id,
+			action: {
+				label: 'Update',
+				onClick: () => {
+					downloadAndInstall(update);
+				}
+			}
+		});
+	} else {
+		if (isAuto) return;
+		toast.success('No updates available', {
+			id,
+			description: `You are using the latest version ${await getVersion()}`,
+			cancel: {
+				label: 'Ok',
+				onClick: () => {}
+			}
+		});
+	}
+}
+
+export async function downloadAndInstall(update: Update) {
+	const id = toast.info(`Update ${update.version} is available. Downloading...`);
+	let downloaded = 0;
+	let contentLength = 0;
+	await update.downloadAndInstall((event) => {
+		switch (event.event) {
+			case 'Started':
+				contentLength = (event.data.contentLength ?? 0) / 1e6;
+				toast.loading(`Downloading update ${update.version}...`, {
+					id,
+					description: `Started downloading ${contentLength.toFixed(2)} MB`
+				});
+				break;
+			case 'Progress':
+				downloaded += event.data.chunkLength;
+				toast.loading(`Downloading update ${update.version}...`, {
+					id,
+					description: `Downloaded ${(downloaded / 1e6).toFixed(2)} / ${contentLength.toFixed(2)} MB`
+				});
+
+				break;
+			case 'Finished':
+				toast.success(`Update ${update.version} downloaded. Installing...`, {
+					id
+				});
+				break;
+		}
+	});
+	toast.success(`Update ${update.version} installed. Relaunching...`, {
+		id
+	});
+	await relaunch();
+}
