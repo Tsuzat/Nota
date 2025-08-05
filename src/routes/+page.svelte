dd47acb6a8f877a29cf100cfbdff15cde2bdab40
Tsuzat <dev.tsuzat@gmail.com>
2025-08-05 23:26:06 +0530

    fix(layout): integrate ToggleMode component and adjust main layout structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import ArtifactDownloader from '$lib/artifact/artifact-downloader.svelte';
+	import ToggleMode from '$lib/components/custom/toggle-mode.svelte';
 	import { Button } from '$lib/components/ui/button';
 	import { Github } from '@lucide/svelte';
 </script>
 
-<main class="flex h-screen w-screen flex-col items-center justify-center gap-8">
-	<span class="bg-muted text-muted-foreground rounded-lg p-2"> BETA </span>
-	<div class="flex items-center gap-4">
+<ToggleMode />
+
+<main class="flex size-full flex-col items-center justify-center gap-8 overflow-auto">
+	<div class="relative flex items-center gap-4">
+		<span class="text-muted-foreground absolute bottom-1 left-1 rounded-full p-1 text-sm">
+			BETA
+		</span>
 		<img src="/favicon.webp" alt="Nota" class="mb-4 size-18" />
 		<h1 class="animate-pulse text-7xl font-bold">Nota</h1>
 	</div>
@@ -20,4 +25,15 @@
 		</Button>
 		<ArtifactDownloader />
 	</div>
+	<img
+		class="hidden aspect-auto h-auto max-w-4xl dark:block"
+		src="https://github.com/user-attachments/assets/ceeaf70d-bf58-4b9e-96bd-f23f2b3f2235"
+		alt="nota"
+	/>
+
+	<img
+		class="block aspect-auto h-auto max-w-4xl dark:hidden"
+		src="https://github.com/user-attachments/assets/2b1265a0-babc-47ad-aad6-f62b36e5f681"
+		alt="nota"
+	/>
 </main>
