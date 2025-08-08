ed0a7e0bc31b02f53c1342194ad96e92b2244d46
Tsuzat <dev.tsuzat@gmail.com>
2025-08-08 21:58:28 +0530

    Add Supabase authentication and session management

---
+++ src/lib/supabase/index.ts
@@ -0,0 +1,8 @@
+import { PUBLIC_SUPABASE_API_KEY, PUBLIC_SUPABASE_ENDPOINT } from '$env/static/public';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = PUBLIC_SUPABASE_ENDPOINT;
+const supabaseKey = PUBLIC_SUPABASE_API_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const auth = supabase.auth;
---
+++ src/lib/supabase/user.svelte.ts
@@ -0,0 +1,38 @@
+import type { User, Session } from '@supabase/supabase-js';
+import { getContext, setContext } from 'svelte';
+
+class SessionAndUser {
+	#user = $state<User | null>(null);
+	#session = $state<Session | null>(null);
+
+	constructor(user: User | null, session: Session | null) {
+		this.#user = user;
+		this.#session = session;
+	}
+
+	getUser() {
+		return this.#user;
+	}
+
+	getSession() {
+		return this.#session;
+	}
+
+	setUser(user: User | null) {
+		this.#user = user;
+	}
+
+	setSession(session: Session | null) {
+		this.#session = session;
+	}
+}
+
+const SESSIONANDUSERKEY = Symbol('SESSIONANDUSERKEY');
+
+export const setSessionAndUserContext = () => {
+	return setContext(SESSIONANDUSERKEY, new SessionAndUser(null, null));
+};
+
+export const getSessionAndUserContext = () => {
+	return getContext<ReturnType<typeof setSessionAndUserContext>>(SESSIONANDUSERKEY);
+};
