f08ea2ba19f7ef1bdc8791a3555caffe2830058b
Tsuzat <dev.tsuzat@gmail.com>
2025-08-08 21:58:37 +0530

    Add auth flow and user profile menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,48 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import { onMount } from 'svelte';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { supabase } from '$lib/supabase';
+	import { setSessionAndUserContext } from '$lib/supabase/user.svelte';
+	import { invalidate } from '$app/navigation';
+	import { setGlobalSignInContext, GlobalSignIn } from '$lib/components/custom/global-signin';
+
+	setGlobalSignInContext();
+
+	const sessionAndUser = setSessionAndUserContext();
 
 	let { children } = $props();
 
-	onMount(() => {});
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, newSession) => {
+			const session = sessionAndUser.getSession();
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+				sessionAndUser.setSession(null);
+				sessionAndUser.setUser(null);
+			} else if (newSession?.user !== session?.user) {
+				invalidate('supabase:auth');
+				sessionAndUser.setSession(null);
+				sessionAndUser.setUser(null);
+			}
+			if (event === 'INITIAL_SESSION' && newSession) {
+				sessionAndUser.setSession(newSession);
+				sessionAndUser.setUser(newSession?.user);
+			} else if (event === 'SIGNED_IN' && newSession) {
+				sessionAndUser.setSession(newSession);
+				sessionAndUser.setUser(newSession?.user);
+			} else if (event === 'SIGNED_OUT' && newSession) {
+				invalidate('supabase:auth');
+				sessionAndUser.setSession(null);
+				sessionAndUser.setUser(null);
+			}
+		});
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 <Toaster richColors closeButton />
 
+<GlobalSignIn />
 <ModeWatcher />
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,66 @@
 <script lang="ts">
 	import ArtifactDownloader from '$lib/artifact/artifact-downloader.svelte';
+	import SimpleTooltip from '$lib/components/custom/simple-tooltip.svelte';
 	import ToggleMode from '$lib/components/custom/toggle-mode.svelte';
 	import { Button } from '$lib/components/ui/button';
-	import { Github } from '@lucide/svelte';
+	import { auth } from '$lib/supabase';
+	import { getSessionAndUserContext } from '$lib/supabase/user.svelte';
+	import { Github, LogOut, User, UserRound } from '@lucide/svelte';
+	import * as Dropdown from '$lib/components/ui/dropdown-menu';
+	import { getGlobalSignInContext } from '$lib/components/custom/global-signin';
+	import { toast } from 'svelte-sonner';
+
+	const user = $derived(getSessionAndUserContext().getUser());
+
+	const globalSignInContext = getGlobalSignInContext();
 </script>
 
-<ToggleMode />
-
-<main class="flex size-full flex-col items-center justify-center gap-8 overflow-auto">
-	<div class="relative flex items-center gap-4">
-		<span class="text-muted-foreground absolute bottom-1 left-1 rounded-full p-1 text-sm">
-			BETA
-		</span>
-		<img src="/favicon.webp" alt="Nota" class="mb-4 size-18" />
-		<h1 class="animate-pulse text-7xl font-bold">Nota</h1>
+<header class="sticky top-1 mx-auto flex max-w-4xl items-center justify-between gap-4 p-4">
+	<a class="flex items-center gap-4" href="/">
+		<img src="/favicon.webp" alt="Nota" class="size-10" />
+		<h3 class="font-bold">Nota</h3>
+	</a>
+	<div class="flex items-center gap-4">
+		<ToggleMode />
+		{#if user === null}
+			<Button onclick={() => (globalSignInContext.open = true)}>Sign In</Button>
+		{:else}
+			<Dropdown.Root>
+				<Dropdown.Trigger>
+					<SimpleTooltip content={user.email}>
+						<Button onclick={async () => await auth.signOut()} size="icon" variant="ghost">
+							<User />
+						</Button>
+					</SimpleTooltip>
+				</Dropdown.Trigger>
+				<Dropdown.Content class="w-fit">
+					<Dropdown.Label class="text-xs">
+						{user.email}
+					</Dropdown.Label>
+					<Dropdown.Item>
+						<UserRound />
+						<span>Profile</span>
+					</Dropdown.Item>
+					<Dropdown.Item
+						onclick={() =>
+							toast.promise(auth.signOut(), {
+								loading: 'Signing you out...',
+								success: 'Signed out successfully.',
+								error: 'Something went wrong'
+							})}
+					>
+						<LogOut />
+						Sign Out
+					</Dropdown.Item>
+				</Dropdown.Content>
+			</Dropdown.Root>
+		{/if}
 	</div>
+</header>
+
+<main
+	class="mx-auto flex size-full max-w-4xl flex-col items-center justify-center gap-8 overflow-auto pt-8"
+>
 	<p class="animate-bounce">
 		A fast, modern, feature rich, lightweight and local first note taking desktop application
 	</p>
@@ -26,13 +72,13 @@
 		<ArtifactDownloader />
 	</div>
 	<img
-		class="hidden aspect-auto h-auto max-w-4xl dark:block"
+		class="hidden aspect-auto h-auto w-full dark:block"
 		src="https://github.com/user-attachments/assets/ceeaf70d-bf58-4b9e-96bd-f23f2b3f2235"
 		alt="nota"
 	/>
 
 	<img
-		class="block aspect-auto h-auto max-w-4xl dark:hidden"
+		class="block aspect-auto h-auto w-full dark:hidden"
 		src="https://github.com/user-attachments/assets/2b1265a0-babc-47ad-aad6-f62b36e5f681"
 		alt="nota"
 	/>
