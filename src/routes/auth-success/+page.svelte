17475234d548de7f93c6d55cdb4b7cbeaa7e6243
Tsuzat <dev.tsuzat@gmail.com>
2025-08-19 09:18:47 +0530

    feat(auth): handle desktop auth callback and update UI
    Add deep link redirect for desktop auth flow and reduce counter duration
    Animate success message and fix typo in redirect text

--- src/routes/auth-success/+page.svelte
+++ src/routes/auth-success/+page.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import { onMount } from 'svelte';
 
-	let counter = $state(5);
+	let counter = $state(3);
 
 	onMount(() => {
+		const searchParams = page.url.searchParams;
+		const desktop = searchParams.get('desktop') === 'true';
+		const code = searchParams.get('code');
+		if (desktop) {
+			const deepLink = `nota://auth/callback?code=${code}`;
+			window.location.href = deepLink;
+		}
 		const interval = setInterval(() => {
 			counter -= 1;
 			if (counter <= 0) {
@@ -15,7 +23,7 @@
 </script>
 
 <div data-tauri-drag-handle class="flex h-screen w-full flex-col items-center justify-center gap-4">
-	<h2>Auth Success</h2>
+	<h2 class="animate-bounce font-semibold">Auth Success</h2>
 	<div>
 		You'll be redired to
 		<a class="text-primary underline" href="/">home</a>
