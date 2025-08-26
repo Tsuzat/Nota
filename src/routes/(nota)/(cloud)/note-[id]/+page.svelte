e021acfc3b16f83c64cdb923aaa8a98aaa71486c
Tsuzat <dev.tsuzat@gmail.com>
2025-08-26 23:44:49 +0530

    feat(cloud): add note detail page with dynamic id routing
    Implement dynamic routing for note details by creating a new page component that displays the note ID. Includes header with navigation controls and responsive sidebar handling.

---
+++ src/routes/(nota)/(cloud)/note-[id]/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import BackAndForthButtons from '$lib/components/custom/back-and-forth-buttons.svelte';
+	import { Separator } from '$lib/components/ui/separator';
+	import { SidebarTrigger, useSidebar } from '$lib/components/ui/sidebar';
+	import { cn, ISMACOS, ISTAURI } from '$lib/utils';
+
+	const { data } = $props();
+	const sidebar = useSidebar();
+</script>
+
+<header class="flex h-12 shrink-0 items-center gap-2">
+	<div
+		class={cn(
+			'z-20 ml-18 flex items-center gap-2 px-3',
+			ISMACOS && !sidebar.open && 'ml-18',
+			ISMACOS && ISTAURI && sidebar.open && 'md:ml-0'
+		)}
+	>
+		<SidebarTrigger />
+		<BackAndForthButtons />
+		<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+	</div>
+</header>
+
+<div class="flex h-[calc(100vh-3rem)] flex-1 flex-grow flex-col overflow-auto">
+	<h3>{data.id}</h3>
+</div>
