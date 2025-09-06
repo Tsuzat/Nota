86612ae658d4798d6074acd3869665c7fae8b045
Tsuzat <dev.tsuzat@gmail.com>
2025-09-06 22:58:35 +0530

    Add support for window decorations in Windows environment

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,14 @@
 import { initializeLocalDB } from '$lib/local/db';
-import { ISTAURI } from '$lib/utils';
+import { ISTAURI, ISWINDOWS } from '$lib/utils';
+import { getCurrentWindow } from '@tauri-apps/api/window';
 
 const init = async () => {
 	if (ISTAURI) {
 		await initializeLocalDB();
+		if (ISWINDOWS) {
+			const window = getCurrentWindow();
+			await window.setDecorations(false);
+		}
 	}
 };
 
