67c4c2135d012068ce1d1d25393574ea79d16ae1
Tsuzat <dev.tsuzat@gmail.com>
2025-11-13 12:45:09 +0530

    feat(sidebar): add tooltip with keyboard shortcut to sidebar trigger
    Update tiptap dependencies and svelte version to latest patches

--- src/lib/components/ui/sidebar/sidebar-trigger.svelte
+++ src/lib/components/ui/sidebar/sidebar-trigger.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index.js';
-	import { cn } from '$lib/utils.js';
+	import { cn, getKeyboardShortcut } from '$lib/utils.js';
 	import PanelLeftIcon from '@lucide/svelte/icons/panel-left';
 	import type { ComponentProps } from 'svelte';
 	import { useSidebar } from './context.svelte.js';
+	import SimpleTooltip from '$lib/components/custom/simple-tooltip.svelte';
 
 	let {
 		ref = $bindable(null),
@@ -17,19 +18,29 @@
 	const sidebar = useSidebar();
 </script>
 
-<Button
-	data-sidebar="trigger"
-	data-slot="sidebar-trigger"
-	variant="ghost"
-	size="icon"
-	class={cn('size-7', className)}
-	type="button"
-	onclick={(e) => {
-		onclick?.(e);
-		sidebar.toggle();
-	}}
-	{...restProps}
->
-	<PanelLeftIcon />
-	<span class="sr-only">Toggle Sidebar</span>
-</Button>
+<SimpleTooltip>
+	<Button
+		data-sidebar="trigger"
+		data-slot="sidebar-trigger"
+		variant="ghost"
+		size="icon"
+		class={cn('size-7', className)}
+		type="button"
+		onclick={(e) => {
+			onclick?.(e);
+			sidebar.toggle();
+		}}
+		{...restProps}
+	>
+		<PanelLeftIcon />
+		<span class="sr-only">Toggle Sidebar</span>
+	</Button>
+	{#snippet child()}
+		<span
+			>Toggle Sidebar
+			<span class="bg-background text-primary rounded p-0.5">
+				{getKeyboardShortcut('\\', true)}
+			</span>
+		</span>
+	{/snippet}
+</SimpleTooltip>
