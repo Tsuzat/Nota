298bddd1381580eec138c913eb95bdeba33d8ccd
Tsuzat <dev.tsuzat@gmail.com>
2025-11-24 15:09:13 +0530

    refactor(auth): consolidate auth functions into index.ts
    Move signInWithEmailAndPassword and signOut functions to index.ts for better organization

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,5 +1,7 @@
+import { invalidate } from '$app/navigation';
 import { PUBLIC_SUPABASE_API_KEY, PUBLIC_SUPABASE_ENDPOINT } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
+import { toast } from 'svelte-sonner';
 
 const supabaseUrl = PUBLIC_SUPABASE_ENDPOINT;
 const supabaseKey = PUBLIC_SUPABASE_API_KEY;
@@ -10,5 +12,19 @@ export const supabase = createClient(supabaseUrl, supabaseKey, {
 		autoRefreshToken: true
 	}
 });
+
+export function signOut() {
+	toast.promise(supabase.auth.signOut, {
+		loading: 'Signing you out...',
+		success: () => {
+			invalidate('supabase:auth');
+			return 'Signed Out Successfully';
+		},
+		error: (err) => {
+			console.error(err);
+			return 'Failed to sign out';
+		}
+	});
+}
 
 export const auth = supabase.auth;
