141362cd8bc105b15fdec400a2a0dba5492d4795
Tsuzat <dev.tsuzat@gmail.com>
2025-12-11 01:03:27 +0530

    fix(sidebar): adjust logo size and handle window resize for tauri apps
    update app logo size from 6 to 8 for better visibility
    add window resize listener to handle fullscreen state in tauri apps

--- src/lib/components/custom/side-bar/app-sidebar.svelte
+++ src/lib/components/custom/side-bar/app-sidebar.svelte
@@ -4,23 +4,33 @@
 	import NavSecondary from './nav-secondary.svelte';
 	import TeamSwitcher from './userworkspace-switcher.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar';
-	import type { ComponentProps } from 'svelte';
+	import { onMount, type ComponentProps } from 'svelte';
 	import NavWorkspacesLocal from './nav-workspaces-local.svelte';
 	import { ISMACOS, ISTAURI } from '$lib/utils';
 	import ToggleMode from '../toggle-mode.svelte';
 	import { useCurrentUserWorkspaceContext } from '../user-workspace/userworkspace.svelte';
 	import NavWorkspacesCloud from './nav-workspaces-cloud.svelte';
 	import AppLogoMenu from '../app-logo-menu.svelte';
+	import { APPWINDOW } from '$lib/contants';
 
 	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
 	const currentUserWorkspace = $derived(useCurrentUserWorkspaceContext());
+
+	let IS_MAXIMUM = $state(false);
+
+	onMount(async () => {
+		IS_MAXIMUM = await APPWINDOW.isFullscreen();
+		APPWINDOW.listen('tauri://resize', async () => {
+			IS_MAXIMUM = await APPWINDOW.isFullscreen();
+		});
+	});
 </script>
 
 <Sidebar.Root bind:ref class="border-r-0" {...restProps}>
 	<Sidebar.Header>
 		{#if ISTAURI}
 			<div data-tauri-drag-region class="flex h-8 items-center justify-between">
-				{#if ISMACOS}
+				{#if ISMACOS && ISTAURI && !IS_MAXIMUM}
 					<div></div>
 				{:else}
 					<AppLogoMenu />
