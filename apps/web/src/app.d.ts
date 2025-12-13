0d57b45e7938e4c089a6ff6ad44df3446a24917e
Tsuzat <dev.tsuzat@gmail.com>
2025-12-13 23:26:32 +0530

    feat: Integrate Supabase authentication, Sentry error monitoring, and Vercel analytics and speed insights.

--- apps/web/src/app.d.ts
+++ apps/web/src/app.d.ts
@@ -1,10 +1,22 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+import { SupabaseClient, Session } from "@supabase/supabase-js";
+
 declare global {
   namespace App {
-    // interface Error {}
-    // interface Locals {}
-    // interface PageData {}
+    interface Error {
+      server: { message: string; stack?: string; status?: number };
+      client: { message: string; stack?: string; status?: number };
+    }
+    interface Locals {
+      supabase: SupabaseClient;
+      safeGetSession(): Promise<{ session: Session | null; user: User | null }>;
+    }
+    interface PageData {
+      session: Session | null;
+      user: User | null;
+    }
     // interface PageState {}
     // interface Platform {}
   }
---
+++ apps/web/src/lib/sentry/index.ts
@@ -0,0 +1,6 @@
+import { logger } from "@sentry/sveltekit";
+
+export const loginfo = logger.info;
+export const logerror = logger.error;
+export const logwarn = logger.warn;
+export const logdebug = logger.debug;
