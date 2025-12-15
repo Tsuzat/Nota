a2024a16fc4743ed0ea349024ef3ef57c5bb40af
Tsuzat <dev.tsuzat@gmail.com>
2025-12-15 21:23:30 +0530

    chore: update app icon and version, adjust menu button styling
    - Update app icon image
    - Bump version to 0.9.42-beta in Cargo.lock
    - Simplify menu button class and size props

--- apps/desktop/src/lib/components/app-menu.svelte
+++ apps/desktop/src/lib/components/app-menu.svelte
@@ -9,6 +9,6 @@ async function showMenu() {
 }
 </script>
 
-<Button variant="ghost" size="icon" class="z-10 size-8 p-0.5" onclick={showMenu}>
+<Button variant="ghost" size="icon-sm" class="z-10" onclick={showMenu}>
 	<img {src} alt="AppLogo" class="aspect-square" />
 </Button>
