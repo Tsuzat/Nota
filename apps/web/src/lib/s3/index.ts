62dfd8b16d1753c79fc8f2cb93d2b3e477158c24
Tsuzat <dev.tsuzat@gmail.com>
2025-12-23 13:12:03 +0530

    feat(storage): add S3 file storage integration with API endpoints
    implement S3 client for file operations and create API endpoints for upload, list and delete
    add required dependencies and environment configuration

---
+++ apps/web/src/lib/s3/index.ts
@@ -0,0 +1,80 @@
+import { DeleteObjectsCommand, ListObjectsV2Command, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import {
+  BUCKET_NAME,
+  R2_ACCESS_ID,
+  R2_ACCOUNT_ID,
+  R2_PUBLIC_ENDPOINT,
+  R2_SECRETE_ACCESS_KEY,
+} from '$env/static/private';
+
+export const S3 = new S3Client({
+  region: 'auto',
+  endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  credentials: {
+    accessKeyId: R2_ACCESS_ID,
+    secretAccessKey: R2_SECRETE_ACCESS_KEY,
+  },
+});
+
+export const uploadFile = async (file: File, key: string) => {
+  const arrayBuffer = await file.arrayBuffer();
+  const buffer = new Uint8Array(arrayBuffer);
+  const command = new PutObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: key,
+    Body: buffer,
+    ContentType: file.type,
+    ACL: 'public-read',
+  });
+
+  try {
+    const data = await S3.send(command);
+    if (data.$metadata.httpStatusCode !== 200) {
+      throw new Error('Failed to upload file');
+    }
+    return `${R2_PUBLIC_ENDPOINT}/${key}`;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+/**
+ * Delete a file from the bucket
+ * @param key file key that starts with the bucket name
+ */
+export const deleteFiles = async (keys: string[]) => {
+  const command = new DeleteObjectsCommand({
+    Bucket: BUCKET_NAME,
+    Delete: {
+      Objects: keys.map((key) => ({ Key: key })),
+    },
+  });
+  const data = await S3.send(command);
+  if (data.$metadata.httpStatusCode !== 200) {
+    throw new Error('Failed to delete file');
+  }
+};
+
+/**
+ * List all files in the bucket that match the filter
+ * @param filter filter by file type
+ * @returns list of file keys that match the filter
+ */
+export const listFiles = async () => {
+  const command = new ListObjectsV2Command({
+    Bucket: BUCKET_NAME,
+  });
+  const data = await S3.send(command);
+  if (data.$metadata.httpStatusCode !== 200) {
+    throw new Error('Failed to list files');
+  }
+  const files: string[] = [];
+  for (const item of data.Contents || []) {
+    const key = item.Key;
+    if (key) {
+      files.push(`${R2_PUBLIC_ENDPOINT}/${key}`);
+    }
+  }
+  return files;
+};
---
+++ apps/web/src/routes/api/storage/+server.ts
@@ -0,0 +1,95 @@
+import type { User } from '@supabase/supabase-js';
+import { json } from '@sveltejs/kit';
+import { nanoid } from 'nanoid';
+import { R2_PUBLIC_ENDPOINT } from '$env/static/private';
+import { deleteFiles, listFiles, uploadFile } from '$lib/s3/index.js';
+import { adminClient } from '$lib/supabase/admin/index.js';
+
+export const config = {
+  runtime: 'bun1.x',
+};
+
+const getUserFromToken = async (token: string) => {
+  const { data: userData, error: userError } = await adminClient.auth.getUser(token);
+  if (userError) {
+    console.error(userError);
+    return null;
+  }
+  return userData.user;
+};
+
+export const POST = async ({ request, locals }) => {
+  let user: User | null = null;
+  const authHeader = request.headers.get('Authorization');
+  const token = authHeader ? authHeader.split(' ')[1] : undefined;
+  if (token) {
+    user = await getUserFromToken(token);
+  } else {
+    user = (await locals.safeGetSession()).user || null;
+  }
+  if (user === null) {
+    return new Response('Unauthorized', { status: 401 });
+  }
+  try {
+    const files = await listFiles();
+    return json({ files });
+  } catch (err) {
+    console.error(err);
+    return new Response('Failed to list all files', { status: 500 });
+  }
+};
+
+export const PUT = async ({ request, locals }) => {
+  let user: User | null = null;
+  const authHeader = request.headers.get('Authorization');
+  const token = authHeader ? authHeader.split(' ')[1] : undefined;
+  if (token) {
+    user = await getUserFromToken(token);
+  } else {
+    user = (await locals.safeGetSession()).user || null;
+  }
+  if (user === null) {
+    return new Response('Unauthorized', { status: 401 });
+  }
+  //! how can we use a json here ??
+  const formData = await request.formData();
+  const file = formData.get('file') as File;
+  if (!file) {
+    return new Response('No file uploaded', { status: 400 });
+  }
+  try {
+    const fileName = `${user.id}/${nanoid(12)}.${file.type.split('/')[1]}`;
+    const url = await uploadFile(file, fileName);
+    return json({ url });
+  } catch (err) {
+    console.error(err);
+    return new Response('Failed to upload file', { status: 500 });
+  }
+};
+
+export const DELETE = async ({ request, locals }) => {
+  // get the token from request header
+  let user: User | null = null;
+  const authHeader = request.headers.get('Authorization');
+  const token = authHeader ? authHeader.split(' ')[1] : undefined;
+  if (token) {
+    user = await getUserFromToken(token);
+  } else {
+    user = (await locals.safeGetSession()).user || null;
+  }
+  if (user === null) {
+    return new Response('Unauthorized', { status: 401 });
+  }
+  const { fileUrls }: { fileUrls: string[] } = await request.json();
+  if (!fileUrls) {
+    return new Response('No fileUrl provided', { status: 400 });
+  }
+  try {
+    const fileKeys = fileUrls.map((url) => url.split(`${R2_PUBLIC_ENDPOINT}/`)[1]);
+    await deleteFiles(fileKeys);
+    return new Response(JSON.stringify({ message: 'File deleted successfully' }), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response('Failed to delete file', { status: 500 });
+  }
+};
