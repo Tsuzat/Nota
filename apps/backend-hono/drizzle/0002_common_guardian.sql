6475f93b8250b7e0edaf3fd10b6f9bd04d3a6d7b
Tsuzat <dev.tsuzat@gmail.com>
2026-02-16 10:48:05 +0530

    remove bun hono backend

--- apps/backend-hono/drizzle/0002_common_guardian.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE "sessions" CASCADE;--> statement-breakpoint
-ALTER TABLE "userworkspaces" RENAME COLUMN "icons" TO "icon";--> statement-breakpoint
-ALTER TABLE "notes" ADD COLUMN "is_public" boolean DEFAULT false;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "encrypted_password" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "email_verified" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "email_verified_at" timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "email_verification_token" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "email_verification_token_expires_at" timestamp with time zone;
--- apps/backend-hono/src/app/middlewares/checkpro.ts
+++
@@ -1,9 +0,0 @@
-import type { Context, Next } from 'hono';
-
-export const proMiddleWare = async (c: Context, next: Next) => {
-  const user = c.get('user');
-  if (user.subscriptionPlan === 'free') {
-    return c.json({ error: 'Pro feature is not available for free users' }, 403);
-  }
-  await next();
-};
