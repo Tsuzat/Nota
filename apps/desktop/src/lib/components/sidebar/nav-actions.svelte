ecc95020ed5479255d74948d945e07d8f385a69a
Tsuzat <dev.tsuzat@gmail.com>
2026-03-19 01:26:51 +0530

    feat: Add Callout editor extension and enhance sidebar with lock page, word count, and last edited information.

--- apps/desktop/src/lib/components/sidebar/nav-actions.svelte
+++ apps/desktop/src/lib/components/sidebar/nav-actions.svelte
@@ -8,7 +8,7 @@ import * as Dropdown from '@nota/ui/shadcn/dropdown-menu';
 import { cn } from '@nota/ui/utils';
 import { getLocalNotes, type LocalNote } from '$lib/local/notes.svelte';
 import { getLocalWorkspaces } from '$lib/local/workspaces.svelte';
-import { exportContent, importNotes } from '$lib/utils';
+import { exportContent, importNotes, timeAgo } from '$lib/utils';
 import { getGlobalSettings } from '../settings';
 
 interface Props {
@@ -33,101 +33,118 @@ let open = $state(false);
 </script>
 
 <div class="flex items-center gap-2 text-sm">
-	<SimpleToolTip content="Toggle Favorite">
-		<Button variant="ghost" size="icon-sm"  onclick={toggleStar}>
-			<icons.Star class={cn(starred && 'fill-yellow-500 text-yellow-500')} />
-		</Button>
-	</SimpleToolTip>
-	<Dropdown.Root bind:open>
-		<Dropdown.Trigger
-			class={buttonVariants({
-				variant: 'ghost',
-				size: 'icon-sm',
-				class: 'data-[state=open]:bg-accent'
-			})}
-		>
-			<icons.Ellipsis />
-		</Dropdown.Trigger>
-		<Dropdown.Content class="bg-popover h-full w-fit overflow-auto" align="end">
-			<Dropdown.Group>
-				<Dropdown.GroupHeading class="text-muted-foreground text-sm"
-					>Page Settings
-				</Dropdown.GroupHeading>
-				<Dropdown.CheckboxItem bind:checked={globalSettings.useToolBar}>
-					<icons.PenTool />
-					Toolbar
-				</Dropdown.CheckboxItem>
-				<Dropdown.CheckboxItem bind:checked={globalSettings.useBubbleMenu}>
-					<icons.Bubbles />
-					Bubble Menu
-				</Dropdown.CheckboxItem>
-				<Dropdown.CheckboxItem bind:checked={globalSettings.useDragHandle}>
-					<icons.GripVertical />	
-					Drag Handle
-				</Dropdown.CheckboxItem>
-			</Dropdown.Group>
-			<Dropdown.Separator />
-			<Dropdown.Group>
-				{#if 'owner' in note}
-					<Dropdown.Item onclick={() => cloudNotes.update(note.id, { is_public: !note.is_public})}>
-						<icons.Globe />
-						{note.is_public ? 'Make Private' : 'Make Public'}
-					</Dropdown.Item>
-				{/if}
-				<Dropdown.Item
-					onclick={() => {
-						if ('owner' in note) cloudNotes.duplicate(note.id);
-						else if (workspace) localNotes.duplicateNote(workspace, note);
-					}}
-				>
-					<icons.Copy />
-					Duplicate
-				</Dropdown.Item>
-				<Dropdown.Item onclick={() => importNotes(editor)}>
-					<icons.ArrowDown />
-					Import
-				</Dropdown.Item>
-				<Dropdown.Sub>
-					<Dropdown.SubTrigger>
-						<icons.ArrowRightFromLine />
-						Export As
-					</Dropdown.SubTrigger>
-					<Dropdown.SubContent>
-						<Dropdown.Item
-							onclick={() => {
-								if (editor) exportContent(editor, note.name, 'JSON');
-							}}>JSON</Dropdown.Item
-						>
-						<Dropdown.Item
-							onclick={() => {
-								if (editor) exportContent(editor, note.name, 'HTML');
-							}}>HTML</Dropdown.Item
-						>
-						<Dropdown.Item
-							onclick={() => {
-								if (editor) exportContent(editor, note.name, 'TEXT');
-							}}>Text</Dropdown.Item
-						>
-						<Dropdown.Item
-							onclick={() => {
-								if (editor) exportContent(editor, note.name, 'MD');
-							}}>Markdown</Dropdown.Item
-						>
-					</Dropdown.SubContent>
-				</Dropdown.Sub>
-			</Dropdown.Group>
-			<Dropdown.Separator />
-			<Dropdown.Group>
-				<Dropdown.Item
-					onclick={() => {
-						if ('owner' in note) cloudNotes.update(note.id, { trashed: true });
-						else localNotes.trashNote(note);
-					}}
-				>
-					<icons.Trash2 />
-					<span>Move to Trash</span>
-				</Dropdown.Item>
-			</Dropdown.Group>
-		</Dropdown.Content>
-	</Dropdown.Root>
+  <SimpleToolTip content="Toggle Favorite">
+    <Button variant="ghost" size="icon-sm" onclick={toggleStar}>
+      <icons.Star class={cn(starred && "fill-yellow-500 text-yellow-500")} />
+    </Button>
+  </SimpleToolTip>
+  <Dropdown.Root bind:open>
+    <Dropdown.Trigger
+      class={buttonVariants({
+        variant: "ghost",
+        size: "icon-sm",
+        class: "data-[state=open]:bg-accent",
+      })}
+    >
+      <icons.Ellipsis />
+    </Dropdown.Trigger>
+    <Dropdown.Content class="bg-popover h-full w-fit overflow-auto" align="end">
+      <Dropdown.Group>
+        <Dropdown.GroupHeading class="text-muted-foreground text-sm"
+          >Page Settings
+        </Dropdown.GroupHeading>
+        <Dropdown.CheckboxItem
+          onclick={() => editor?.setEditable(!editor?.isEditable)}
+          checked={!editor?.isEditable}
+        >
+          <icons.Lock />
+          Lock Page
+        </Dropdown.CheckboxItem>
+        <Dropdown.CheckboxItem bind:checked={globalSettings.useToolBar}>
+          <icons.PenTool />
+          Toolbar
+        </Dropdown.CheckboxItem>
+        <Dropdown.CheckboxItem bind:checked={globalSettings.useBubbleMenu}>
+          <icons.Bubbles />
+          Bubble Menu
+        </Dropdown.CheckboxItem>
+        <Dropdown.CheckboxItem bind:checked={globalSettings.useDragHandle}>
+          <icons.GripVertical />
+          Drag Handle
+        </Dropdown.CheckboxItem>
+      </Dropdown.Group>
+      <Dropdown.Separator />
+      <Dropdown.Group>
+        {#if "owner" in note}
+          <Dropdown.Item
+            onclick={() =>
+              cloudNotes.update(note.id, { is_public: !note.is_public })}
+          >
+            <icons.Globe />
+            {note.is_public ? "Make Private" : "Make Public"}
+          </Dropdown.Item>
+        {/if}
+        <Dropdown.Item
+          onclick={() => {
+            if ("owner" in note) cloudNotes.duplicate(note.id);
+            else if (workspace) localNotes.duplicateNote(workspace, note);
+          }}
+        >
+          <icons.Copy />
+          Duplicate
+        </Dropdown.Item>
+        <Dropdown.Item onclick={() => importNotes(editor)}>
+          <icons.ArrowDown />
+          Import
+        </Dropdown.Item>
+        <Dropdown.Sub>
+          <Dropdown.SubTrigger>
+            <icons.ArrowRightFromLine />
+            Export As
+          </Dropdown.SubTrigger>
+          <Dropdown.SubContent>
+            <Dropdown.Item
+              onclick={() => {
+                if (editor) exportContent(editor, note.name, "JSON");
+              }}>JSON</Dropdown.Item
+            >
+            <Dropdown.Item
+              onclick={() => {
+                if (editor) exportContent(editor, note.name, "HTML");
+              }}>HTML</Dropdown.Item
+            >
+            <Dropdown.Item
+              onclick={() => {
+                if (editor) exportContent(editor, note.name, "TEXT");
+              }}>Text</Dropdown.Item
+            >
+            <Dropdown.Item
+              onclick={() => {
+                if (editor) exportContent(editor, note.name, "MD");
+              }}>Markdown</Dropdown.Item
+            >
+          </Dropdown.SubContent>
+        </Dropdown.Sub>
+      </Dropdown.Group>
+      <Dropdown.Separator />
+      <Dropdown.Group>
+        <Dropdown.Item
+          onclick={() => {
+            if ("owner" in note) cloudNotes.update(note.id, { trashed: true });
+            else localNotes.trashNote(note);
+          }}
+        >
+          <icons.Trash2 />
+          <span>Move to Trash</span>
+        </Dropdown.Item>
+      </Dropdown.Group>
+      <Dropdown.Separator />
+      <Dropdown.Label class="font-normal text-sm text-muted-foreground">
+        Word count: {editor?.storage.characterCount.words()}
+      </Dropdown.Label>
+      <Dropdown.Label class="font-normal text-sm text-muted-foreground">
+        Last Edited: {timeAgo(note.updated_at)}
+      </Dropdown.Label>
+    </Dropdown.Content>
+  </Dropdown.Root>
 </div>
