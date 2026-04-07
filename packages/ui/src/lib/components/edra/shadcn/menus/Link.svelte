83351b1ca349ea98893d15dc2f90ae721cb4800a
Tsuzat <dev.tsuzat@gmail.com>
2026-04-07 21:21:15 +0530

    refactor: upgrade lucide-svelte, update tooltip styles, and improve link bubble menu UI

--- packages/ui/src/lib/components/edra/shadcn/menus/Link.svelte
+++ packages/ui/src/lib/components/edra/shadcn/menus/Link.svelte
@@ -6,9 +6,10 @@ import Check from '@lucide/svelte/icons/check';
 import Copy from '@lucide/svelte/icons/copy';
 import Edit from '@lucide/svelte/icons/edit';
 import Trash from '@lucide/svelte/icons/trash';
+import ExternalLink from '@lucide/svelte/icons/external-link';
 import type { Editor } from '@tiptap/core';
 import BubbleMenu from '../../components/BubbleMenu.svelte';
-import type { ShouldShowProps } from '../../types';
+import SimpleToolTip from '@lib/components/custom/SimpleToolTip.svelte';
 
 interface Props {
   editor: Editor;
@@ -31,78 +32,86 @@ function handleSubmit(e: Event) {
 </script>
 
 <BubbleMenu
-	{editor}
-	pluginKey="link-bubble-menu"
-	shouldShow={(props: ShouldShowProps) => {
-		if (props.editor.isActive('link')) {
-			return true;
-		} else {
-			isEditing = false;
-			linkInput = undefined;
-			return false;
-		}
-	}}
-	options={{
-		shift: true,
-		autoPlacement: {
-			allowedPlacements: ['top', 'top-end', 'top-start']
-		},
-		strategy: 'absolute',
-		scrollTarget: parentElement
-	}}
-	class="bg-popover flex h-fit w-fit items-center gap-1 rounded-lg border p-0!"
+  {editor}
+  pluginKey="link-bubble-menu"
+  shouldShow={(props: ShouldShowProps) => {
+    if (props.editor.isActive("link")) {
+      return true;
+    } else {
+      isEditing = false;
+      linkInput = undefined;
+      return false;
+    }
+  }}
+  options={{
+    shift: true,
+    autoPlacement: {
+      allowedPlacements: ["top", "top-end", "top-start"],
+    },
+    strategy: "absolute",
+    scrollTarget: parentElement,
+  }}
+  class="bg-popover flex h-fit w-fit items-center gap-0.5 rounded-lg border p-0!"
 >
-	{#if !isEditing}
-		<Button
-			variant="link"
-			href={link}
-			class="max-w-120 truncate overflow-hidden p-1 text-ellipsis underline"
-			target="_blank"
-		>
-			{link}
-		</Button>
-		<SimpleTooltip content="Edit Link">
-			<Button
-				variant="ghost"
-				size="icon"
-				onclick={() => {
-					isEditing = true;
-					editor.commands.blur();
-				}}
-			>
-				<Edit />
-			</Button>
-		</SimpleTooltip>
-		<SimpleTooltip content="Copy Link">
-			<Button
-				variant="ghost"
-				title="Copy Link"
-				size="icon"
-				onclick={() => {
-					window.navigator.clipboard.writeText(link);
-				}}
-			>
-				<Copy />
-			</Button>
-		</SimpleTooltip>
-		<SimpleTooltip content="Remove Link">
-			<Button
-				variant="ghost"
-				title="Remove Link"
-				size="icon"
-				onclick={() => editor.chain().focus().extendMarkRange('link').unsetLink().run()}
-			>
-				<Trash />
-			</Button>
-		</SimpleTooltip>
-	{:else}
-		<form onsubmit={handleSubmit} class="flex max-w-120 items-center gap-0.5">
-			<Input bind:value={linkInput} required type="url" placeholder="Type or paste a link" />
-			<SimpleTooltip content="Set new link">
-				<Button type="submit" size="icon">
-					<Check />
-				</Button>
-			</SimpleTooltip>
-		</form>
-	{/if}
+  {#if !isEditing}
+    <SimpleToolTip content="Open Link in browser">
+      <Button
+        variant="link"
+        href={link}
+        class="max-w-120 truncate overflow-hidden p-1 text-ellipsis underline"
+        target="_blank"
+      >
+        <ExternalLink />
+      </Button>
+    </SimpleToolTip>
+    <SimpleTooltip content="Edit Link">
+      <Button
+        variant="ghost"
+        size="icon"
+        onclick={() => {
+          isEditing = true;
+          editor.commands.blur();
+        }}
+      >
+        <Edit />
+      </Button>
+    </SimpleTooltip>
+    <SimpleTooltip content="Copy Link">
+      <Button
+        variant="ghost"
+        title="Copy Link"
+        size="icon"
+        onclick={() => {
+          window.navigator.clipboard.writeText(link);
+        }}
+      >
+        <Copy />
+      </Button>
+    </SimpleTooltip>
+    <SimpleTooltip content="Remove Link">
+      <Button
+        variant="ghost"
+        title="Remove Link"
+        size="icon"
+        onclick={() =>
+          editor.chain().focus().extendMarkRange("link").unsetLink().run()}
+      >
+        <Trash />
+      </Button>
+    </SimpleTooltip>
+  {:else}
+    <form onsubmit={handleSubmit} class="flex max-w-120 items-center gap-0.5">
+      <Input
+        bind:value={linkInput}
+        required
+        type="url"
+        placeholder="Type or paste a link"
+      />
+      <SimpleTooltip content="Set new link">
+        <Button type="submit" size="icon">
+          <Check />
+        </Button>
+      </SimpleTooltip>
+    </form>
+  {/if}
 </BubbleMenu>
