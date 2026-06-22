ace324c925c541a64de1f5964d42650d58bd65bb
Tsuzat <dev.tsuzat@gmail.com>
2026-06-22 11:49:39 +0530

    refactor: unify request logic by integrating custom fetch into the apiRequest client module

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,15 +1,16 @@
 import { type Handle, redirect } from "@sveltejs/kit";
 import { PUBLIC_BACKEND_URL } from "$env/static/public";
+import { request as apiRequest } from "@nota/client";
 
 export const handle: Handle = async ({ event, resolve }) => {
   const url = `${PUBLIC_BACKEND_URL}/api/v1/user/me`;
-  const res = await event.fetch(url, {
-    method: "GET",
+  const res = await apiRequest(url, {
     headers: event.request.headers,
+    fetch: event.fetch,
   });
   if (res.ok) {
     try {
-      const data = await res.json();
+      const data: any = await res.json();
       event.locals.user = data.data;
     } catch (error) {
       console.error("Error fetching user data:", error);
