186b2e71d956bb7813d77df33a7bc9a50b84facf
Tsuzat <dev.tsuzat@gmail.com>
2026-08-16 23:53:04 +0530

    feat(ui): add icons renderer and collapsible components, update select exports

--- packages/ui/src/lib/components/icons/index.ts
+++ packages/ui/src/lib/components/icons/index.ts
@@ -1,1 +1,2 @@
 export { default as BarSpinner } from "./bar-spinner.svelte";
+export { default as IconsRenderer } from "./icons-renderer.svelte";
--- packages/ui/src/lib/components/ui/select/select-item.svelte
+++ packages/ui/src/lib/components/ui/select/select-item.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-import { Select as SelectPrimitive } from "bits-ui";
 import CheckIcon from "@lucide/svelte/icons/check";
+import { Select as SelectPrimitive } from "bits-ui";
 import { cn, type WithoutChild } from "#lib/utils.js";
 
 let {
